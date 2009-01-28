4f32d08d3cfbfbad52385a896951919f649a6ea1
Joshua Sierles <joshua@rasgueado.local>
2009-01-28 17:03:13 +0100

    use /var for chef path

--- config/solo.rb
+++ config/solo.rb
@@ -1,8 +1,8 @@
-cookbook_path     "/etc/chef/cookbooks"
-node_path         "/etc/chef/nodes"
+cookbook_path     "/var/chef/cookbooks"
+node_path         "/var/chef/nodes"
 #search_index_path File.join(File.dirname(__FILE__), "search_index")
 log_level         :info
-file_store_path  "/etc/chef"
+file_store_path  "/var/chef"
 file_cache_path  "/var/chef/cache"
 node_name        `hostname -s`
 
