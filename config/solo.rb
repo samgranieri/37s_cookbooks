4c183b1f798295ab7638b9872507a5c4d9b38fc3
Joshua Sierles <joshua@rasgueado.local>
2009-01-28 18:24:56 +0100

    make cache and cookbook path the same

--- config/solo.rb
+++ config/solo.rb
@@ -3,7 +3,7 @@ node_path         "/var/chef/nodes"
 #search_index_path File.join(File.dirname(__FILE__), "search_index")
 log_level         :info
 file_store_path  "/var/chef"
-file_cache_path  "/var/chef/cache"
+file_cache_path  "/var/chef"
 node_name        `hostname -s`
 
 Chef::Log::Formatter.show_time = false
