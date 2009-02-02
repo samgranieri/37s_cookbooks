6188e2bd033e91b47c0215de7717f3acff50fc85
Joshua Sierles <joshua@rasgueado.local>
2009-02-02 18:06:46 +0100

    mash attributes require normal assignment syntax

--- cookbooks/base/attributes/default.rb
+++ cookbooks/base/attributes/default.rb
@@ -1,4 +1,4 @@
 base Mash.new unless attribute?("base")
 
-base[:sysadmin_email] "sysadmins@37signals.com"
-base[:sysadmin_sms_email] "sysadmins@37signals.com"
+base[:sysadmin_email] = "sysadmins@37signals.com"
+base[:sysadmin_sms_email] = "sysadmins@37signals.com"
