7c4f9c31867c5ef80f60251448e14be74db9b635
Joshua Sierles <joshua@rasgueado.local>
2009-02-04 14:00:37 +0100

    use default nagiosadmin username

--- cookbooks/nagios/attributes/server.rb
+++ cookbooks/nagios/attributes/server.rb
@@ -3,4 +3,4 @@ nagios Mash.new unless attribute?("nagios")
 nagios[:root] = "/etc/nagios3"
 nagios[:config_subdir] = "conf.d"
 nagios[:users] = Mash.new
-nagios[:users]["admin"] = "casp3end6e"
+nagios[:users]["nagiosadmin"] = "casp3end6e"
