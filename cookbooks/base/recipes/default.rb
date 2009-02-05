fe225a8b898618e673dac6e0d551ff2854e5847a
Joshua Sierles <joshua@rasgueado.local>
2009-02-05 15:27:46 +0100

    workaround groupadd bug

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -1,15 +1,15 @@
-node[:groups].each do |g|
-  group g[:name] do
-    gid g[:gid]
-  end
-end
+# node[:groups].each do |g|
+#   group g[:name] do
+#     gid g[:gid]
+#   end
+# end
 
 node[:users].each do |u|
-  user u[:name] do
+  user u[:username] do
     comment u[:comment]
     uid u[:uid]
     gid u[:gid]
-    shell "bin/bash"
+    shell "/bin/bash"
     password u[:password]
   end
 end
