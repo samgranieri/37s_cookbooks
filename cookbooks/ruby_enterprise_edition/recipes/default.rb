2615ad2e5e031e68537fecedac997ab2eecdcd0f
Joshua Sierles <joshua@rasgueado.local>
2009-02-06 17:03:27 +0100

    add shadow library for compiling against REE

--- cookbooks/ruby_enterprise_edition/recipes/default.rb
+++ cookbooks/ruby_enterprise_edition/recipes/default.rb
@@ -21,3 +21,22 @@ bash "install_ruby_enterprise_edition" do
   only_if @node[:ruby_enterprise_edition][:only_if]
 end
 
+filename = @node[:ruby_enterprise_edition][:url].split("/").last
+name = 'shadow-1.4.1'
+
+remote_file "/usr/local/src/#{filename}" do
+  source filename
+  not_if File.exists? "/usr/local/lib/ruby/site_ruby/1.8/x86_64-linux/shadow.so"
+end
+
+bash "install_ruby_enterprise_edition" do
+  user "root"
+  cwd "/usr/local/src"
+  code <<-EOH
+  tar -zxf #{filename}
+  cd #{name}
+  ruby extconf.rb
+  make install
+  EOH
+  not_if File.exists? "/usr/local/lib/ruby/site_ruby/1.8/x86_64-linux/shadow.so"
+end
