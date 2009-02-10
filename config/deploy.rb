c27bbfd75563c2998ef6160c4eeb34ccac0c1173
Joshua Sierles <joshua@rasgueado.local>
2009-02-10 12:53:25 +0100

    added cap config

---
+++ config/deploy.rb
@@ -0,0 +1,54 @@
+set :application, "chef_recipes"
+set :scm_user, ENV['USER']
+set :repository, "ssh://#{scm_user}@dev.37signals.com/u/git/chef_recipes.git"
+set :deploy_to, "/var/chef_recipes"
+set :deploy_via, :fast_remote_cache
+set :scm, :git
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+require 'capistrano/ext/multistage'
+
+task :fix_git_remote_url do
+  if scm == :git && strategy.is_a?(Capistrano::Deploy::Strategy::RemoteCache)
+    run "if [ -d #{shared_path}/cached-copy ]; then cd #{shared_path}/cached-copy; git config remote.origin.url #{repository}; fi"
+  else
+    logger.important "#{current_task.fully_qualified_name} is only run for the git SCM and remote-cache"
+  end
+end
+
+desc "Install essential packages for chef"
+task :bootstrap do
+  run "apt-get update"
+  run "DEBCONF_TERSE='yes' DEBIAN_PRIORITY='critical' DEBIAN_FRONTEND=noninteractive apt-get -qyu install emacs22-nox vim git-core mysql-client libshadow-ruby1.8 libmysqlclient15-dev build-essential syslog-ng ntp ruby ruby1.8-dev rubygems libopenssl-ruby1.8 irb rdoc ri rubygems man rake"
+  run "cd /tmp && wget http://rubyforge.org/frs/download.php/45904/rubygems-update-1.3.1.gem && sudo gem install rubygems-update-1.3.1.gem && sudo /var/lib/gems/1.8/bin/update_rubygems"
+  run "mkdir -p /etc/chef"
+  run "sudo gem sources -a http://gems.opscode.com && gem sources -a http://gems.github.com"
+  run "gem install opscode-ohai opscode-chef --no-rdoc --no-ri"
+end
+
+desc "Install latest chef"
+task :install_chef do
+  run "gem install rspec json --no-rdoc --no-ri"
+  #run "git clone git://github.com/opscode/ohai.git"
+  #run "cd ohai && rake install"
+  run "git clone git://github.com/opscode/chef.git"
+  run "cd chef && rake install"
+end
+
+desc "Update chef"
+task :update_chef do
+  run "cd ~/chef && git pull && rake install"
+  run "cd ~/ohai && git pull && rake install"
+end
+
+before "deploy:setup", :bootstrap
+
+before "deploy", :fix_git_remote_url
+after "deploy:update", "deploy:cleanup"
+
+deploy.task :default, :except => {:no_release => true} do
+  on_rollback { run "rm -rf #{release_path}; true" }
+  strategy.deploy!
+  symlink
+end
---
+++ vendor/plugins/cached_externals/recipes/cached_externals.rb
@@ -0,0 +1,61 @@
+set(:external_modules) do
+  require 'yaml'
+
+  modules = YAML.load_file("config/externals.yml") rescue {}
+  modules.each do |path, options|
+    strings = options.select { |k, v| String === k }
+    raise ArgumentError, "the externals.yml file must use symbols for the option keys (found #{strings.inspect} under #{path})" if strings.any?
+  end
+end
+
+desc "Indicate that externals should be applied locally. See externals:setup."
+task :local do
+  set :stage, :local
+end
+
+namespace :externals do
+  desc <<-DESC
+    Set up all defined external modules. This will check to see if any of the
+    modules need to be checked out (be they new or just updated), and will then
+    create symlinks to them. If running in 'local' mode (see the :local task)
+    then these will be created in a "../shared/externals" directory relative
+    to the project root. Otherwise, these will be created on the remote
+    machines under [shared_path]/externals.
+  DESC
+  task :setup, :except => { :no_release => true } do
+    require 'capistrano/recipes/deploy/scm'
+
+    external_modules.each do |path, options|
+      puts "configuring #{path}"
+      scm = Capistrano::Deploy::SCM.new(options[:type], options)
+      revision = scm.query_revision(options[:revision]) { |cmd| `#{cmd}` }
+
+      if exists?(:stage) && stage == :local
+        FileUtils.rm_rf(path)
+        shared = File.expand_path(File.join("../shared/externals", path))
+        FileUtils.mkdir_p(shared)
+        destination = File.join(shared, revision)
+        if !File.exists?(destination)
+          unless system(scm.checkout(revision, destination))
+            FileUtils.rm_rf(destination) if File.exists?(destination)
+            raise "Error checking out #{revision} to #{destination}"
+          end
+        end
+        FileUtils.ln_s(destination, path)
+      else
+        shared = File.join(shared_path, "externals", path)
+        destination = File.join(shared, revision)
+        run "rm -rf #{latest_release}/#{path} && mkdir -p #{shared} && if [ ! -d #{destination} ]; then (#{scm.checkout(revision, destination)}) || rm -rf #{destination}; fi && ln -nsf #{destination} #{latest_release}/#{path}"
+      end
+    end
+  end
+end
+
+# Need to do this before finalize_update, instead of after update_code,
+# because finalize_update tries to do a touch of all assets, and some
+# assets might be symlinks to files in plugins that have been externalized.
+# Updating those externals after finalize_update means that the plugins
+# haven't been set up yet when the touch occurs, causing the touch to
+# fail and leaving some assets temporally out of sync, potentially, with
+# the other servers.
+before "deploy:finalize_update", "externals:setup"
