b25f21e8392eed5119377bef8d62e7e2e6b7e9ef
Joshua Sierles <joshua@rasgueado.local>
2009-02-10 18:44:05 +0100

    add sample json and new ec2 host

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,11 +21,16 @@ desc "Install essential packages for chef"
 task :bootstrap do
   run "apt-get update"
   run "DEBCONF_TERSE='yes' DEBIAN_PRIORITY='critical' DEBIAN_FRONTEND=noninteractive apt-get -qyu install emacs22-nox vim git-core mysql-client libshadow-ruby1.8 libmysqlclient15-dev build-essential syslog-ng ntp ruby ruby1.8-dev rubygems libopenssl-ruby1.8 irb rdoc ri rubygems man rake"
-  run "cd /tmp && wget http://rubyforge.org/frs/download.php/45904/rubygems-update-1.3.1.gem && sudo gem install rubygems-update-1.3.1.gem && sudo /var/lib/gems/1.8/bin/update_rubygems"
-  run "mkdir -p /etc/chef"
+  install_rubygems
+  run "mkdir -p /etc/chef /var/chef /var/chef/merb/log"
   run "sudo gem sources -a http://gems.opscode.com && gem sources -a http://gems.github.com"
   install_chef
   
+end
+
+desc "Install rubygems"
+task :install_rubygems do
+  run "cd /tmp && wget http://rubyforge.org/frs/download.php/45904/rubygems-update-1.3.1.gem && sudo gem install rubygems-update-1.3.1.gem && sudo /var/lib/gems/1.8/bin/update_rubygems"
 end
 
 desc "Install latest chef"
--- config/deploy/ec2-eu.rb
+++ config/deploy/ec2-eu.rb
@@ -1,3 +1,3 @@
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ec2", "europe"), File.join(ENV["HOME"], ".ssh", "id_rsa")] 
 set :user, "root"
-role :app, "ec2-79-125-53-60.eu-west-1.compute.amazonaws.com"
+role :app, "ec2-79-125-53-122.eu-west-1.compute.amazonaws.com"
