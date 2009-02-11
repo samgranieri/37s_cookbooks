31f607a99e4ec9abe55407a11d7921e37873e3f9
Joshua Sierles <joshua@rasgueado.local>
2009-02-11 15:15:52 +0100

    use local gems path

--- cookbooks/chef/attributes/server.rb
+++ cookbooks/chef/attributes/server.rb
@@ -1,4 +1,4 @@
-recipes << "base"
+gems_path `gem env gemdir`.chomp!
 chef Mash.new unless attribute?("chef")
 chef[:server_version] = "0.5.3"
-chef[:server_path] = "#{node[:base][:gems_path]}/chef-server-#{chef[:server_version]}"
+chef[:server_path] = "#{gems_path}/chef-server-#{chef[:server_version]}"
