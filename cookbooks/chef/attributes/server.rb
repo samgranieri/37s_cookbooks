0070eddb842976c887b55fd3afa71d08a0c90d89
Joshua Sierles <joshua@rasgueado.local>
2009-02-11 17:16:42 +0100

    fix gempath

--- cookbooks/chef/attributes/server.rb
+++ cookbooks/chef/attributes/server.rb
@@ -1,4 +1,4 @@
 gems_path `gem env gemdir`.chomp!
 chef Mash.new unless attribute?("chef")
 chef[:server_version] = "0.5.3"
-chef[:server_path] = "#{gems_path}/chef-server-#{chef[:server_version]}"
+chef[:server_path] = "#{gems_path}/gems/chef-server-#{chef[:server_version]}"
