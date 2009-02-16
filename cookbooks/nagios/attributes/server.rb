54366e2c55fce33be666ab09acf3efef8559289b
Joshua Sierles <joshua@rasgueado.local>
2009-02-16 19:40:29 +0100

    add more nagios.cfg options

--- cookbooks/nagios/attributes/server.rb
+++ cookbooks/nagios/attributes/server.rb
@@ -4,3 +4,10 @@ nagios[:root] = "/etc/nagios3"
 nagios[:config_subdir] = "conf.d"
 nagios[:users] = Mash.new
 nagios[:users]["nagiosadmin"] = "casp3end6e"
+nagios[:notifications_enabled] = true
+
+# This setting is effectively sets the minimum interval (in seconds) nagios can handle.
+# Other interval settings provided in seconds will calculate their actual from this value, since nagios works in 'time units' rather than allowing definitions everywhere in seconds
+
+nagios[:interval_length] = 15
+
