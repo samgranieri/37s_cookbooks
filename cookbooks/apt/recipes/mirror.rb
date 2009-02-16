eddc8374bf9967533a799939ee218cd7425f98b0
Joshua Sierles <joshua@rasgueado.local>
2009-02-16 20:29:03 +0100

    create necessary directories for apt mirror

--- cookbooks/apt/recipes/mirror.rb
+++ cookbooks/apt/recipes/mirror.rb
@@ -6,3 +6,19 @@ directory node[:apt][:base_path] do
   group "root"
   mode 0755
 end
+
+%(var skel mirror).each do |dir|
+  directory node[:apt][:base_path]+"/#{dir}" do
+    action :create
+    owner "root"
+    group "root"
+    mode 0755
+  end
+end
+
+cron "ganglia_tomcat_thread_max" do
+  command "/usr/bin/apt-mirror > /var/log/apt-mirror.log 2>&1"
+  hour 5
+  only_if File.exist?(node[:apt][:base_path]+"/mirror")
+end
+
