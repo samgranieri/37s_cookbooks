b507336c60ba065e98d8e33ddffe11e816469e37
Joshua Sierles <joshua@rasgueado.local>
2009-02-16 19:41:08 +0100

    add external_commands attribute

--- cookbooks/nagios/attributes/server.rb
+++ cookbooks/nagios/attributes/server.rb
@@ -5,6 +5,7 @@ nagios[:config_subdir] = "conf.d"
 nagios[:users] = Mash.new
 nagios[:users]["nagiosadmin"] = "casp3end6e"
 nagios[:notifications_enabled] = true
+nagios[:check_external_commands] = true
 
 # This setting is effectively sets the minimum interval (in seconds) nagios can handle.
 # Other interval settings provided in seconds will calculate their actual from this value, since nagios works in 'time units' rather than allowing definitions everywhere in seconds
