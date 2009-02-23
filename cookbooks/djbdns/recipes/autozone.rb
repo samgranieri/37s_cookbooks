f27e2a59f5d079ad90f37b35a0166d523738db7f
Joshua Sierles <joshua@rasgueado.local>
2009-02-23 17:49:59 +0100

    add building and verifying for separate tinydns zone file

---
+++ cookbooks/djbdns/recipes/autozone.rb
@@ -0,0 +1,12 @@
+# This recipe will generate a zone file from the chef server's node list. Requires chef-client
+# It also automatically generates an authoritative zone record for the current domain and an A record for 'chef' pointing to the current server
+
+hosts = []
+search(:node, "*") {|n| hosts << n }
+
+template "/etc/tinydns-internal/zones/chef-server.zone" do
+  source "tinydns-internal-data.erb"
+  mode 644
+  variables(:hosts => hosts)
+  notifies :run, resources("execute[build-tinydns-internal-data]")
+end
--- cookbooks/djbdns/recipes/internal_server.rb
+++ cookbooks/djbdns/recipes/internal_server.rb
@@ -25,22 +25,22 @@ end
 
 execute "build-tinydns-internal-data" do
   cwd "/etc/tinydns-internal/root"
-  command "make"
+  command "./update_from_zones && make"
   action :nothing
-end
-
-hosts = []
-search(:node, "*") {|n| hosts << n }
-
-template "/etc/tinydns-internal/root/data" do
-  source "tinydns-internal-data.erb"
-  mode 644
-  variables(:hosts => hosts)
-  notifies :run, resources("execute[build-tinydns-internal-data]")
 end
 
 template "/etc/tinydns-internal/run" do
   source "sv-server-run.erb"
+  mode 0755
+end
+
+template "/etc/tinydns-internal/root/update_from_zones" do
+  source "update_from_zones.sh.erb"
+  mode 0755
+end
+
+remote_file "/etc/tinydns-internal/root/valtz" do
+  source "valtz"
   mode 0755
 end
 
@@ -55,4 +55,12 @@ end
 
 link "/etc/init.d/tinydns-internal" do
   to node[:runit_sv_bin]
+end
+
+directory "/etc/tinydns-internal/zones" do
+  mode 0755
+end
+
+directory "/etc/tinydns-internal/backup-data" do
+  mode 0755
 end
