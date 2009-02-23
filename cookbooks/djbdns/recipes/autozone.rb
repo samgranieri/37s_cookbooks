4b05ca1b6d00afb2f7c7377bb44ded6d3398c7f6
Joshua Sierles <joshua@rasgueado.local>
2009-02-23 21:04:25 +0100

    include tinydns server recipe for zone autoconfig

--- cookbooks/djbdns/recipes/autozone.rb
+++ cookbooks/djbdns/recipes/autozone.rb
@@ -1,6 +1,8 @@
 # This recipe will generate a zone file from the chef server's node list. Requires chef-client
 # It also automatically generates an authoritative zone record for the current domain and an A record for 'chef' pointing to the current server
 
+include_recipe "djbdns::internal_server"
+
 hosts = []
 search(:node, "*") {|n| hosts << n }
 
