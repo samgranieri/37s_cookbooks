bc09b36d661ba10a02050dc440862de096b07120
Joshua Sierles <joshua@rasgueado.local>
2009-02-24 19:55:18 +0100

    require a node attribute

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -1,53 +1,48 @@
+
+raise RuntimeError, "The node requires a role, one of: #{node[:roles].keys.join(',')}" unless node[:role] and node[:roles].has_key?(node[:role])
+
 include_recipe "build-essential"
 require_recipe "postfix"
 require_recipe "ssh::server"
 include_recipe "ssh_keys"
 
-if node[:role] and node[:roles].has_key?(node[:role])
-  
-  role[:groups].each do |group_name|
-  
-    group group_name.to_s do
-      gid node[:groups][group_name][:gid]
-    end
+role[:groups].each do |group_name|
 
-    users = node[:users].find_all { |u| u.last[:group] == group_name }
-
-    users.each do |u, config|
-      user u do
-        comment config[:comment]
-        uid config[:uid]
-        gid config[:group].to_s
-        home "/home/#{u}"
-        shell "/bin/bash"
-        password config[:password]
-        supports :manage_home => true
-      end
-
-      directory "/home/#{u}/.ssh" do
-        action :create
-        owner u
-        group config[:group].to_s
-        mode 0700
-      end
-      
-      add_keys u do
-        conf config
-      end
-    end
+  group group_name.to_s do
+    gid node[:groups][group_name][:gid]
   end
 
-  directory "/u" do
-    action :create
-    owner "root"
-    group "admin"
-    mode 0775
+  users = node[:users].find_all { |u| u.last[:group] == group_name }
+
+  users.each do |u, config|
+    user u do
+      comment config[:comment]
+      uid config[:uid]
+      gid config[:group].to_s
+      home "/home/#{u}"
+      shell "/bin/bash"
+      password config[:password]
+      supports :manage_home => true
+    end
+
+    directory "/home/#{u}/.ssh" do
+      action :create
+      owner u
+      group config[:group].to_s
+      mode 0700
+    end
+    
+    add_keys u do
+      conf config
+    end
   end
-
-  
-  
-
-  require_recipe "sudo"
-else
-  Chef::Log.warn "The node requires a role, one of: #{node[:roles].keys.join(',')}"
 end
+
+directory "/u" do
+  action :create
+  owner "root"
+  group "admin"
+  mode 0775
+end
+
+require_recipe "sudo"
