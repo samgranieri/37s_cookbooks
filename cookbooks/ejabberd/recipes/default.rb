f544452a0a2c59af448740ef6790941d265f7876
Joshua Sierles <joshua@rasgueado.local>
2009-02-25 19:29:40 +0100

    typo

--- cookbooks/ejabberd/recipes/default.rb
+++ cookbooks/ejabberd/recipes/default.rb
@@ -1,8 +1,10 @@
+package "ejabberd"
+
 template "/etc/ejabberd/ejabberd.cfg" do
   source "ejabberd.cfg.erb"
   variables(:jabber_domain => node[:base][:jabber_domain])
 end
 
-execute "add ejabberd admin user"
+execute "add ejabberd admin user" do
   command "ejabberdctl register admin #{node[:jabber_domain]} #{node[:base][:jabber_admin_password]}"
 end
