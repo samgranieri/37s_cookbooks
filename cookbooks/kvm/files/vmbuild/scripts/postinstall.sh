2273bc508a9a82688e4af477c2b2439d7da413bb
Mark Imbriaco <mark@imbriaco.com>
2009-03-01 23:56:52 -0500

    Add basic vmbuild script to Chef.

---
+++ cookbooks/kvm/files/vmbuild/scripts/postinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Enabling ttyS0 console..."
+sh /usr/local/vmbuild/scripts/grub-console.sh $1
--- cookbooks/kvm/recipes/default.rb
+++ cookbooks/kvm/recipes/default.rb
@@ -6,3 +6,14 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+remote_directory "/usr/local/vmbuild" do
+  source "vmbuild"
+  files_backup 2
+  files_owner "root"
+  files_group "admin"
+  files_mode 0640
+  owner "root"
+  group "admin"
+  mode "0750"
+end
