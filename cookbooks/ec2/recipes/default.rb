021223021358e82ab68092d01404d6b630701720
Joshua Sierles <joshua@rasgueado.local>
2009-03-03 16:13:13 +0100

    make ec2 recipe idempotent

--- cookbooks/ec2/recipes/default.rb
+++ cookbooks/ec2/recipes/default.rb
@@ -4,11 +4,13 @@ end
 
 bash "Set hostname" do
   code "echo #{node[:assigned_hostname]}.#{node[:assigned_domain]} > /etc/hostname"
+  not_if "grep #{node[:assigned_hostname]}.#{node[:assigned_domain]} /etc/mailname"
 end
 
 bash "Set mailname for postfix" do
   code "echo #{node[:assigned_hostname]}.#{node[:assigned_domain]} > /etc/mailname"
   notifies :restart, resources(:service => "postfix")
+  not_if "grep #{node[:assigned_hostname]} /etc/mailname"
 end
 
 service "hostname.sh" do
