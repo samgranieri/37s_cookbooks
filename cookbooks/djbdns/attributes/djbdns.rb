b86869b98f1da29fa9dee25f5d7eb70cb052af17
Joshua Sierles <joshua@rasgueado.local>
2009-03-04 20:39:24 +0100

    don't reply on domain attribute existence

--- cookbooks/djbdns/attributes/djbdns.rb
+++ cookbooks/djbdns/attributes/djbdns.rb
@@ -6,4 +6,8 @@ djbdns[:axfrdns_ipaddress] = "127.0.0.1" unless djbdns.has_key?(:axfrdns_ipaddre
 djbdns[:public_dnscache_ipaddress] = ipaddress unless djbdns.has_key?(:public_dnscache_ipaddress)
 # Default allowed networks is the current network class B
 djbdns[:public_dnscache_allowed_networks] = [ipaddress.split(".")[0,2].join(".")] unless djbdns.has_key?(:public_dnscache_allowed_networks)
-djbdns[:tinydns_internal_resolved_domain] = domain unless djbdns.has_key?(:tinydns_internal_resolved_domain)
+
+# this may not exist on a first run of chef-solo on ec2
+if attribute?("domain")
+  djbdns[:tinydns_internal_resolved_domain] = domain unless djbdns.has_key?(:tinydns_internal_resolved_domain)
+end
