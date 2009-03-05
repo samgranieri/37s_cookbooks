a7455f6623af4a15e6a931768f1376fa0bff13b9
Joshua Sierles <joshua@rasgueado.local>
2009-03-05 21:50:52 +0100

    use mount provider for nfs mounts

---
+++ cookbooks/nfs/recipes/client.rb
@@ -0,0 +1,17 @@
+if node[:nfs_mounts]
+  node[:nfs_mounts].each do |target, config|
+    directory target do
+      owner config[:owner]
+      group config[:owner]
+    end
+    mount target do
+      fstype "nfs"
+      options %(rw,soft,intr)
+      device config[:device]
+      dump 0
+      pass 0
+    end
+  end
+else
+  Chef::Log.warn 'You included the NFS client recipe without defining nfs mounts."
+end
