76bd9e0519b8bf7895d40cdbf211ffdce58b032f
Joshua Sierles <joshua@183.Red-88-28-104.staticIP.rima-tde.net>
2009-03-05 16:42:47 +0100

    use the right attributes for ec2

--- cookbooks/ec2/attributes/default.rb
+++ cookbooks/ec2/attributes/default.rb
@@ -1,5 +1,5 @@
-if node[:ec2] && node[:ec2][:userdata]
-  ec2_userdata JSON.parse(node[:userdata])
+if attribute?(:ec2) && ec2[:userdata]
+  ec2_userdata JSON.parse(ec2[:userdata])
   ec2_userdata.each do |k,v|
     send(k.to_s, v)
   end
