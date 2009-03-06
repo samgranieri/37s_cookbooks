992adefa67ded6e0f4a5c79b1e2614853b42b236
Joshua Sierles <joshua@rasgueado.local>
2009-03-06 11:59:46 +0100

    deploy as self

--- config/deploy/ec2-us.rb
+++ config/deploy/ec2-us.rb
@@ -1,3 +1,3 @@
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ec2", "ec2"), File.join(ENV["HOME"], ".ssh", "id_rsa")] 
-set :user, "root"
+set :user, ENV["USER"]
 role :app, "noc.ec2-us.37signals.com"
