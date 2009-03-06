c987b9953de406046c71c3b8df3f2b2021fcf72e
Joshua Sierles <joshua@rasgueado.local>
2009-03-06 13:31:19 +0100

    make client register itself before setting up the runit service

--- cookbooks/chef/recipes/client.rb
+++ cookbooks/chef/recipes/client.rb
@@ -1,2 +1,15 @@
 require_recipe "runit"
+
+template "/etc/chef/client.rb" do
+  owner "chef"
+  group "chef"
+  mode 0644
+  source "client.rb.erb"
+  action :create
+end
+
+execute "Initialize node in chef server" do
+  command "#{node[:chef][:client_path]} -t `cat /etc/chef/validation_token`"
+end
+
 runit_service "chef-client"
