50fd99c8d43ffe7c82c2a99949b2ad5bc560e94e
Mark Imbriaco <mark@imbriaco.com>
2009-03-09 13:39:41 -0400

    Make the install scripts executable.

--- config/deploy/rack-dfw.rb
+++ config/deploy/rack-dfw.rb
@@ -1,5 +1,2 @@
-ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")] 
-set :user, "root"
-set :use_sudo, false
-set :gateway, "#{ENV['USER']}@dev.37signals.com"
+set :gateway, "dev.37signals.com"
 role :app, "noc2"
