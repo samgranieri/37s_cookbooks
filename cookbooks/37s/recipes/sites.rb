475ec9a98e59755cac7163f23f6b7924d5d3797d
Joshua Sierles <joshua@125.Red-88-28-100.staticIP.rima-tde.net>
2009-03-11 13:57:41 +0100

    add site generation recipe

---
+++ cookbooks/37s/recipes/sites.rb
@@ -0,0 +1,15 @@
+node[:active_sites].each do |site, conf|
+
+  full_name = "#{site}_#{conf[:env]}"
+
+  apache_site full_name do
+    config_path "/u/sites/#{site}/current/config/apache/#{conf[:env]}.conf"
+    only_if { File.exists?("/etc/apache2/sites-available/#{full_name}") }
+  end
+
+  logrotate full_name do
+    files "/u/sites/#{site}/current/log/*.log"
+    frequency "weekly"
+    restart_command "/etc/init.d/apache2 reload > /dev/null"
+  end
+end
