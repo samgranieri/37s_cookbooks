a75a3a88fbd417c525a91a9c85547675d3df80c6
Joshua Sierles <joshua@152.Red-88-28-106.staticIP.rima-tde.net>
2009-03-13 19:39:29 +0100

    make sure service starts

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,9 +1,18 @@
 package "nginx"
 
+service "nginx" do
+  action :enable
+  supports :restart => true, :reload => true
+end
+
 file "/etc/nginx/sites-available/default" do
   action :remove
 end
 
 file "/etc/nginx/sites-enabled/default" do
   action :remove
+end
+
+service "nginx" do
+  action :start
 end
