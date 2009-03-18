bad9bfcf3f96db8d8024f847653af70c9e278a29
Joshua Sierles <joshua@rasgueado.local>
2009-03-18 18:08:40 +0100

    add log path to chef client

--- cookbooks/chef/attributes/client.rb
+++ cookbooks/chef/attributes/client.rb
@@ -2,3 +2,4 @@ chef Mash.new unless attribute?("chef")
 chef[:client_path] = `which chef-client`.chomp
 chef[:client_interval] = "300"
 chef[:client_splay] = "20"
+chef[:log_path] = "/var/log/chef/client.log"
