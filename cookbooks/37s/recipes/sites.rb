aa83aeeee6ae32bde3a5f429630b00810858aa27
Joshua Sierles <joshua@rasgueado.local>
2009-03-18 13:48:20 +0100

    sites recipe should check for sites attribute before running

--- cookbooks/37s/recipes/sites.rb
+++ cookbooks/37s/recipes/sites.rb
@@ -8,18 +8,22 @@ directory "/u/logs/sites" do
   group "www-data"
 end
 
-node[:active_sites].each do |site, conf|
+if node[:active_sites]
+  node[:active_sites].each do |site, conf|
 
-  full_name = "#{site}_#{conf[:env]}"
+    full_name = "#{site}_#{conf[:env]}"
 
-  apache_site full_name do
-    config_path "/u/sites/#{site}/current/config/apache/#{conf[:env]}.conf"
-    only_if { File.exists?("/etc/apache2/sites-available/#{full_name}") }
+    apache_site full_name do
+      config_path "/u/sites/#{site}/current/config/apache/#{conf[:env]}.conf"
+      only_if { File.exists?("/etc/apache2/sites-available/#{full_name}") }
+    end
+
+    logrotate full_name do
+      files "/u/sites/#{site}/current/log/*.log"
+      frequency "weekly"
+      restart_command "/etc/init.d/apache2 reload > /dev/null"
+    end
   end
-
-  logrotate full_name do
-    files "/u/sites/#{site}/current/log/*.log"
-    frequency "weekly"
-    restart_command "/etc/init.d/apache2 reload > /dev/null"
-  end
+else
+  Chef::Log.info "Add an 'active_sites' attribute to configure this node's sites"
 end
