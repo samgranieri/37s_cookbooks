c76d7d9534c00a4c706380dac5d275f1b966ca82
Joshua Sierles <joshua@rasgueado.local>
2009-03-20 11:53:19 +0100

    install memcache/libevent debs

--- cookbooks/memcached/recipes/default.rb
+++ cookbooks/memcached/recipes/default.rb
@@ -5,6 +5,16 @@ service "memcached" do
   supports :restart => true, :reload => true
 end
 
+%w(libevent1_1.4.9-1ubuntu1_amd64.deb memcached_1.2.6-1ubuntu2_amd64.deb).each do |f|
+  file "/tmp/#{f}" do
+    source f
+  end
+  execute "install #{f}" do
+    code "dpkg -i /tmp/#{f}"
+    not_if "dpkg -l | grep #{f.match(/.+-(.+).deb/)[1]} | grep ii"
+  end
+end
+
 template node[:memcached][:conf_path] do
   source "memcached.conf.erb"  
   notifies :restart, resources(:service => "memcached")
