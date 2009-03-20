56aa2a9c4758a586cece20f63e388a7876d6c853
Joshua Sierles <joshua@rasgueado.local>
2009-03-20 11:30:03 +0100

    create memcached config template

--- cookbooks/memcached/recipes/default.rb
+++ cookbooks/memcached/recipes/default.rb
@@ -1,6 +1,15 @@
 package "memcached"
 
 service "memcached" do
-  action [:enable, :start]
+  action :enable
   supports :restart => true, :reload => true
+end
+
+template node[:memory][:config_path] do
+  source "memcached.conf.erb"  
+  notifies :restart, resources(:service => "memcached")
+end
+
+service "memcached" do
+  action :start
 end
