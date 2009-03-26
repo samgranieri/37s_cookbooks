1e414f78d0fc51967e01abf36a9ea573d8b82eee
Mark Imbriaco <mark@imbriaco.com>
2009-03-26 10:42:33 -0400

    use the execute resource instead of script, script seems broken.

--- cookbooks/php5/recipes/default.rb
+++ cookbooks/php5/recipes/default.rb
@@ -17,13 +17,9 @@ template "php5.conf" do
   mode 0644
 end
 
-script "install_php" do
-  interpreter "bash"
+execute "install_php" do
+  command = "wget #{node[:php5][:dist_url]} && tar -C /#{node[:php5][:path].split("/")[1..-2].join("/")} -xpf #{node[:php5][:tar_pkg]}"
   user "root"
   cwd "/tmp"
-  code <<-EOC
-wget #{node[:php5][:dist_url]}
-tar -C /#{node[:php5][:path].split("/")[1..-2].join("/")} -xpf #{node[:php5][:tar_pkg]}
-  EOC
-  not_if File.directory?(node[:php5][:path])
+  creates "/usr/local/php"
 end
