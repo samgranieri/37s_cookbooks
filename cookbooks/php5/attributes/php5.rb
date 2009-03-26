350e103e1559e58a55a88d9fccc926098ed5225e
Mark Imbriaco <mark@imbriaco.com>
2009-03-26 10:48:26 -0400

    Fix tar_pkg naming.

--- cookbooks/php5/attributes/php5.rb
+++ cookbooks/php5/attributes/php5.rb
@@ -1,5 +1,5 @@
 php5 Mash.new unless attribute?("php5")
 php5[:path] = "/usr/local/php" unless php5.has_key?(:path)
 php5[:module_path] = File.join(php5[:path], "apache2/libphp5.so") unless php5.has_key?(:module_path)
-php5[:tar_pkg] = "php-#{platform}-#{platform_version}-#{kernel}.tar.bz" unless php5.has_key?(:tar_pkg)
-php5[:dist_url] = "http://dist/packages#{php5[:tar_pkg]}" unless php5.has_key?(:dist_url)
+php5[:tar_pkg] = "php-#{platform}-#{platform_version}-#{kernel[:machine]}.tar.bz" unless php5.has_key?(:tar_pkg)
+php5[:dist_url] = "http://dist/packages/#{php5[:tar_pkg]}" unless php5.has_key?(:dist_url)
