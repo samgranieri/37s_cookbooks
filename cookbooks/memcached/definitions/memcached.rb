219763978d4062cf7f4143f39bad60e2c61fa2a7
Joshua Sierles <joshua@rasgueado.local>
2009-03-31 19:45:05 +0200

    move defualt values inside definition

--- cookbooks/memcached/definitions/memcached.rb
+++ cookbooks/memcached/definitions/memcached.rb
@@ -1,10 +1,10 @@
-define :memcached_instance, :enable => true, :max_memory => @node[:memcached][:max_memory],
-                            :port => @node[:memcached][:port], :user => @node[:memcached][:user],
-                            :max_connections => @node[:memcached][:port]  do
+define :memcached_instance, :enable => true  do
   
   runit_service "memcached-#{params[:name]}" do
     template_name "memcached"
-    options params
+    options {:max_memory => node[:memcached][:max_memory],
+             :port => node[:memcached][:port], :user => node[:memcached][:user],
+             :max_connections => node[:memcached][:port]}.merge(params)
   end
   
   service params[:name] do
