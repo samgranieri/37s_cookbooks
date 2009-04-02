b716d68f535531d275a099865d2ae619c3f2708e
Joshua Sierles <joshua@rasgueado.local>
2009-04-02 13:19:41 +0200

    add queenbee to app list

--- cookbooks/base/attributes/default.rb
+++ cookbooks/base/attributes/default.rb
@@ -108,5 +108,7 @@ applications[:blogcabin] = {:gems => ['fast_xs', 'memcache-client'], :apache_mod
 applications[:tadalist] = {:gems => ['fast_xs']}
 applications[:bookstore] = {:gems => ['fast_xs']}
 applications[:forum] = {:gems => [['ruby-openid', '1.1.4'], 'RedCloth']}
+applications[:queenbee] = {:gems => ['fast_xs']}
+
 
 host_keys[:dev] = "|1|KvHRKrGy/30YCtj0Lc9M5HEwdu8=|b2VJ1rh2Ep44lRyaBSv/Ujrp7g8= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEArjrY9fx5IPyjawRtv1sOu5x8SRvPf+zek//zyPSDxL5up/AfxJZEVUDitJhKbIkSHADX4s9MMueh4QIxftaZPvxQiGG36PczkrxZ0Ob8aIz+pVUvxooe1AOTN/AxGA0AvPDVjJdy1d1PKbxb0w7o4jKFaitxlYp+WlgMBzXCnx7uXDxwtnID/0mICX9d9fnnA8Qf2biFcl2dhQWXiZccsZEaI85CRymNSPHuUOcbZg84No9lEeu5rnMqV8MoJ4VG6GFDdyZyiR/FkmwlTh4Ma9+klfImRQNdN//+fob/JoBpygxzuacT2tcXI/SCYfEoTSbpHE4CvxloRst9Gb8XlQ=="
