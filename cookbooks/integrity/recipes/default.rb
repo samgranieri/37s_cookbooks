85e846261b697c69f5c715839ce6599f0538766b
Mark Imbriaco <mark@imbriaco.com>
2009-04-02 12:37:50 -0400

    Make sure the integrity stuff is owned by app.

--- cookbooks/integrity/recipes/default.rb
+++ cookbooks/integrity/recipes/default.rb
@@ -5,7 +5,15 @@ gem_package "defunkt-integrity-campfire" do
   source "http://gems.github.com"
 end
 
+directory "/u/apps/integrity" do
+  owner "app"
+  group "app"
+  mode 0755
+end
+
 execute "setup_integrity" do
   command "integrity install #{node[:integrity][:path]} --passenger"
+  user "app"
+  group "app"
   not_if "test -e /u/apps/integrity"
 end
