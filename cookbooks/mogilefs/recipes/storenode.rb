72b9f35c1f46e34c8edec61a768c956c5ec07f60
Mark Imbriaco <mark@imbriaco.com>
2009-04-02 00:39:28 -0400

    Create mogstored docroot if it doesn't exist.

--- cookbooks/mogilefs/recipes/storenode.rb
+++ cookbooks/mogilefs/recipes/storenode.rb
@@ -7,3 +7,9 @@ template "#{node[:mogilefs][:path]}/etc/mogstored.conf" do
   mode 0644
 end
 
+directory "#{node[:mogilefs][:mogstored][:doc_root]}" do
+  owner "app"
+  group "app"
+  mode 0755
+end
+
