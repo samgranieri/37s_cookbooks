4362c72b5bc086df8ef80b0a2b664f1a0cc8de54
Mark Imbriaco <mark@imbriaco.com>
2009-04-03 14:20:11 -0400

    Add periodic build script to cron for Integrity projects

---
+++ cookbooks/integrity/files/default/integrity_build.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+# Integrity periodic build script. 
+# Original: http://gist.github.com/88432
+
+ENV["RAILS_ENV"] = "test"
+ 
+require 'rubygems'
+require 'integrity'
+require 'notifier/campfire'
+ 
+Integrity::Notifier.register(Integrity::Notifier::Campfire)
+ 
+Integrity.new(File.dirname(__FILE__) + "/config.yml")
+ 
+Integrity::Project.all.each do |project|
+  last_commit = project.commits.last
+  build = last_commit ? project.send(:head_of_remote_repo) != last_commit.identifier : false
+  project.build if build
+end
+
