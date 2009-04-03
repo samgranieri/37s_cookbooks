c7fe2862f4d3b61cfec05b89cd2e32ed84cda87e
Mark Imbriaco <mark@imbriaco.com>
2009-04-03 15:59:39 -0400

    Explicitly use ruby to run the build cron job.

--- cookbooks/integrity/recipes/default.rb
+++ cookbooks/integrity/recipes/default.rb
@@ -89,7 +89,7 @@ end
 cron "integrity_build" do
   user "app"
   minute "*/10"
-  command "#{node[:integrity][:path]}/integrity_build.rb"
+  command "/usr/local/bin/ruby #{node[:integrity][:path]}/integrity_build.rb"
   only_if { File.exist?(File.join(node[:integrity][:path], "integrity_build.rb")) }
 end
 
