13002705bdad0e8559a9d3f401c59d45f3c91a37
Joshua Sierles <joshua@rasgueado.local>
2009-04-03 20:57:32 +0200

    name module build resources appropriately

--- cookbooks/apache2/recipes/mod_auth_token.rb
+++ cookbooks/apache2/recipes/mod_auth_token.rb
@@ -2,7 +2,7 @@ remote_directory "/usr/local/build/mod_auth_token" do
   source "mod_auth_token"
 end
 
-execute "build and install mod_authn_yubikey" do
+execute "build and install mod_auth_token" do
   command "cd /usr/local/build/mod_auth_token && apxs2 -ci mod_auth_token.c"
   not_if { File.exists?("/usr/lib/apache2/modules/mod_auth_token.so") }
 end
--- cookbooks/apache2/recipes/mod_xsendfile.rb
+++ cookbooks/apache2/recipes/mod_xsendfile.rb
@@ -2,7 +2,7 @@ remote_directory "/usr/local/build/mod_xsendfile" do
   source "mod_xsendfile"
 end
 
-execute "build and install mod_authn_yubikey" do
+execute "build and install mod_xsendfile" do
   command "cd /usr/local/build/mod_xsendfile && apxs2 -ci mod_xsendfile.c"
   not_if { File.exists?("/usr/lib/apache2/modules/mod_xsendfile.so") }
 end
