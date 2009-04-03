249efc62f9599e6f5b1ccdcda0962e67e158df76
Joshua Sierles <joshua@rasgueado.local>
2009-04-03 20:36:26 +0200

    add xsendfile apache module recipe

---
+++ cookbooks/apache2/recipes/mod_xsendfile.rb
@@ -0,0 +1,14 @@
+remote_directory "/usr/local/build/mod_xsendfile" do
+  source "mod_xsendfile"
+end
+
+execute "build and install mod_authn_yubikey" do
+  command "cd /usr/local/build/mod_xsendfile && apxs2 -ci mod_xsendfile.c"
+  not_if { File.exists?("/usr/lib/apache2/modules/mod_xsendfile.so") }
+end
+
+template "/etc/apache2/mods-available/xsendfile.load" do
+  source 'xsendfile.load.erb'
+end
+
+apache_module "xsendfile"
