26732dcb7b5e67fdb1413ddd56016bd26c3f7e24
Joshua Sierles <joshua@rasgueado.local>
2009-04-06 20:07:44 +0200

    set default for log rotation backups to 5

--- cookbooks/logrotate/definitions/logrotate.rb
+++ cookbooks/logrotate/definitions/logrotate.rb
@@ -1,4 +1,4 @@
-define :logrotate, :frequency => "daily", :rotate_count => 10, :rotate_if_empty => false, :missing_ok => true, :compress => true, :enable => true do
+define :logrotate, :frequency => "daily", :rotate_count => 5, :rotate_if_empty => false, :missing_ok => true, :compress => true, :enable => true do
   template "/etc/logrotate.d/#{params[:name]}" do
     action params[:enable] ? :create : :delete
     cookbook "logrotate"
