aa02fdaa6bf5026660a687da46099ea44d12c3ba
Mark Imbriaco <mark@imbriaco.com>
2009-04-09 17:50:38 -0400

    Notify perlbal to restart on config changes.

--- cookbooks/perlbal/recipes/default.rb
+++ cookbooks/perlbal/recipes/default.rb
@@ -7,10 +7,13 @@ directory "#{node[:perlbal][:config_path]}" do
   group "root"
 end
 
+runit_service "perlbal"
+
 template "#{node[:perlbal][:config_path]}/perlbal.conf" do
   source "perlbal.conf.erb"
   owner "app"
   mode 0600
+  notifies :restart, resources(:service => "perlbal")
 end
 
-runit_service "perlbal"
+
