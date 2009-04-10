7b4c9c3382a50fc32d98e1fd86f413152c5c9ea5
Mark Imbriaco <mark@imbriaco.com>
2009-04-10 18:18:08 -0400

    The init script should infer the pid path on it's own.

--- cookbooks/haproxy/recipes/default.rb
+++ cookbooks/haproxy/recipes/default.rb
@@ -43,9 +43,9 @@ node[:haproxy][:instances].each do |instance|
     running true
     
     start_command   "start #{instance[:name]}"
-    stop_command    "stop #{instance[:name]} /var/run/#{instance[:name]}.pid"
-    restart_command "restart #{instance[:name]} /var/run/#{instance[:name]}.pid"
-    reload_command  "reload #{instance[:name]} /var/run/#{instance[:name]}.pid"
+    stop_command    "stop #{instance[:name]}"
+    restart_command "restart #{instance[:name]}"
+    reload_command  "reload #{instance[:name]}"
     
     supports [ :start, :stop, :restart, :reload ]
   end
