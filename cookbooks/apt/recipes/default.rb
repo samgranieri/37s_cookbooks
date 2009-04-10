f446dee1c511cd2814cc9db605edec6259d2dda2
Mark Imbriaco <mark@imbriaco.com>
2009-04-10 14:31:18 -0400

    Fix ordering.  Should create the sources.list before we do an apt update.

--- cookbooks/apt/recipes/default.rb
+++ cookbooks/apt/recipes/default.rb
@@ -1,3 +1,11 @@
+
+remote_file "/etc/apt/sources.list" do
+  source "sources.list"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
 %w{/var/cache/local /var/cache/local/preseeding}.each do |dirname|
   directory dirname do
     owner "root"
@@ -9,12 +17,5 @@ end
 
 execute "apt-get-update" do
   command "apt-get update"
-end
-
-remote_file "/etc/apt/sources.list" do
-  source "sources.list"
-  owner "root"
-  group "root"
-  mode "0644"
 end
 
