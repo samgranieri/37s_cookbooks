872dc5963bef102a632e4c5c02b470ad83f8b596
Mark Imbriaco <mark@imbriaco.com>
2009-04-10 15:40:09 -0400

    First pass at HAproxy cookbook.

---
+++ cookbooks/haproxy/recipes/default.rb
@@ -0,0 +1,41 @@
+
+package "haproxy" do
+  action :install
+end
+
+directory "/etc/haproxy" do
+  action :create
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+remote_file "/etc/init.d/haproxy" do
+  source "haproxy.init"
+  owner "root"
+  group "root"
+  mode 0700
+end
+
+node[:haproxy][:instances].each do |instance|
+  template "/etc/haproxy/#{instance[:name]}.cfg" do
+    source "haproxy.cfg.erb"
+    variables(:instance => instance)
+    owner node[:haproxy][:user]
+    group node[:haproxy][:group]
+    mode 0640
+    notifies :reload, resources(:service => "haproxy_#{instance[:name]}")
+  end
+
+  service "haproxy_#{instance[:name]}" do
+    pattern "haproxy.*#{instance[:name]}"
+    running :true
+    
+    start_command   "start #{instance[:name]}"
+    stop_command    "stop #{instance[:name]} #{instance[:pid_file]}"
+    restart_command "restart #{instance[:name]} #{instance[:pid_file]}"
+    reload_command  "reload #{instance[:name]} #{instance[:pid_file]}"
+    
+    supports [ :start, :stop, :restart, :reload ]
+  end
+end
