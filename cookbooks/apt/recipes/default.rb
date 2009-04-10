d9fab7667fe6eb8f754652e942fd8a88b03b3eda
Mark Imbriaco <mark@imbriaco.com>
2009-04-10 17:32:57 -0400

    Add the public key for the internal apt repository.

--- cookbooks/apt/recipes/default.rb
+++ cookbooks/apt/recipes/default.rb
@@ -1,4 +1,11 @@
 
+remote_file "/etc/apt/packages.gpg" do
+  source "packages.gpg"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
 remote_file "/etc/apt/sources.list" do
   source "sources.list"
   owner "root"
@@ -13,6 +20,11 @@ end
     mode  0644
     action :create
   end
+end
+
+execute "apt-key-add" do
+  command "/usr/bin/apt-key add /etc/apt/packages.gpg"
+  only_if "test `/usr/bin/apt-key list | grep -c packages@37signals.com` -eq 0"
 end
 
 execute "apt-get-update" do
