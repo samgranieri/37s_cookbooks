e39a64da054b7c59a9eb7fc4b22260134449ab87
Mark Imbriaco <mark@imbriaco.com>
2009-04-14 15:33:35 -0400

    Add a tommcat6 service.

--- cookbooks/tomcat/recipes/default.rb
+++ cookbooks/tomcat/recipes/default.rb
@@ -4,6 +4,7 @@ require_recipe "java"
 return unless ["ubuntu", "debian"].include?(node[:platform])
 
 package "tomcat6"
+service "tomcat6"
 
 template "/etc/default/tomcat6" do
   source "default.tomcat6"
