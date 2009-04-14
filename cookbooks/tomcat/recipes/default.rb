f1bf68748e22634288a3ded624dbceadf22b111b
Mark Imbriaco <mark@imbriaco.com>
2009-04-14 17:21:54 -0400

    Need to actually use the template.

--- cookbooks/tomcat/recipes/default.rb
+++ cookbooks/tomcat/recipes/default.rb
@@ -15,6 +15,15 @@ template "/etc/default/tomcat6" do
   notifies :restart, resources(:service => "tomcat6")
 end
 
+template "/etc/tomcat6/server.xml" do
+  source "server.xml.erb"
+  owner "app"
+  group "admin"
+  mode 0644
+  
+  notifies :restart, resources(:service => "tomcat6")
+end
+
 execute "fix_permissions" do
   command "chown -R #{node[:tomcat][:user]}:admin /etc/tomcat6 /var/log/tomcat6 /var/lib/tomcat6 /var/cache/tomcat6 && touch /etc/tomcat6/perms.ok"
   creates "/etc/tomcat6/perms.ok"
