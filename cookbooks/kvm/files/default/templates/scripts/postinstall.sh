dddf8a8e1386e24ccb4f41eed1e0361959d67986
Mark Imbriaco <mark@imbriaco.com>
2009-04-20 12:42:24 -0400

    Rename sda[12] to vda[12] during VM creation.

--- cookbooks/kvm/files/default/templates/scripts/postinstall.sh
+++ cookbooks/kvm/files/default/templates/scripts/postinstall.sh
@@ -2,3 +2,8 @@
 
 echo "Enabling ttyS0 console..."
 sh /usr/local/share/kvm/scripts/grub-console.sh $1
+
+echo "Renaming sda[12] to vda[12] in fstab..."
+sh /usr/local/share/kvm/scripts/rename-fstab-devices.sh $1
+
+
