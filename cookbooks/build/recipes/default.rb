a5af196bfb63b1349580175417239c864c00e5e6
Joshua Sierles <joshua@rasgueado.local>
2009-04-20 18:25:12 +0200

    add some builds

--- cookbooks/build/recipes/default.rb
+++ cookbooks/build/recipes/default.rb
@@ -1,8 +1,7 @@
-#
-# Cookbook Name:: build
-# Recipe:: default
-#
-# Copyright 2009, 37signals
-#
-# All rights reserved - Do Not Redistribute
-#
+gem_package "git_remote_branch"
+
+node[:build].each do |name, config|
+  config[:packages].each do |pack|
+    package pack
+  end
+end
