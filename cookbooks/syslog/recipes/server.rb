e065dbc40ee8f5f6b638d3a7fe7e669041438dc7
Mark Imbriaco <mark@imbriaco.com>
2009-04-26 20:06:59 -0400

    Need to create /u/logs/syslog.

--- cookbooks/syslog/recipes/server.rb
+++ cookbooks/syslog/recipes/server.rb
@@ -16,12 +16,18 @@ end
 
 if node[:applications]
   node[:applications].each do |app, config|
-    directory root+"/#{app}" do
+    directory node[:syslog_ng][:root] + "/#{app}" do
       owner "app"
       group "app"
       mode 0750
     end
   end
+end
+
+directory node[:syslog_ng][:root] + "syslog" do
+  owner "root"
+  group "app"
+  mode 0750
 end
 
 logrotate "applications" do
