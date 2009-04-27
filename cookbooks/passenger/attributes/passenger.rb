01d5037bd1c75645ab388fd0705dfbce3b572eec
Mark Imbriaco <mark@imbriaco.com>
2009-04-27 10:32:12 -0400

    Honor passenger[:version] if it's defined.

--- cookbooks/passenger/attributes/passenger.rb
+++ cookbooks/passenger/attributes/passenger.rb
@@ -2,7 +2,7 @@ gems_path `gem env gemdir`.chomp!
 ruby_path `which ruby`.chomp!
 
 passenger Mash.new unless attribute?("passenger")
-passenger[:version]          = '2.1.3'
+passenger[:version]          = '2.1.3' unless passenger.has_key?(:version)
 passenger[:root_path]        = "#{gems_path}/gems/passenger-#{passenger[:version]}"
 passenger[:module_path]      = "#{passenger[:root_path]}/ext/apache2/mod_passenger.so"
 passenger[:apache_load_path] = '/etc/apache2/mods-available/passenger.load'
