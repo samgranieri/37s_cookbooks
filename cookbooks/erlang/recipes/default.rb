b80d91b7bcdab18405b25ceb245f7bd1b21492bd
Mark Imbriaco <mark@imbriaco.com>
2009-04-28 01:10:16 -0400

    s/create/creates/

--- cookbooks/erlang/recipes/default.rb
+++ cookbooks/erlang/recipes/default.rb
@@ -12,12 +12,12 @@ execute "install mysql library" do
   user "root"
   cwd "/usr/lib/erlang/lib"
   command "curl http://dist/packages/erlang/mysql-02.17.2009.tar.bz2 | tar xfj -"
-  create "/usr/lib/erlang/lib/mysql-02.17.2009/ebin/mysql.beam"
+  creates "/usr/lib/erlang/lib/mysql-02.17.2009/ebin/mysql.beam"
 end
 
 execute "install mochiweb library" do
   user "root"
   cwd "/usr/lib/erlang/lib"
   command "curl http://dist/packages/erlang/mochiweb-02.24.2009.tar.bz2 | tar xfj -"
-  create "/usr/lib/erlang/lib/mochiweb-02.24.2009/ebin/mochiweb.beam"
+  creates "/usr/lib/erlang/lib/mochiweb-02.24.2009/ebin/mochiweb.beam"
 end
