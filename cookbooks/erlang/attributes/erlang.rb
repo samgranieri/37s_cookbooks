7a9643be78d2ab62ff3254be6ae0fc10e5ca1faf
Mark Imbriaco <mark@imbriaco.com>
2009-04-28 00:55:25 -0400

    Set a default cookie.

--- cookbooks/erlang/attributes/erlang.rb
+++ cookbooks/erlang/attributes/erlang.rb
@@ -1,8 +1,10 @@
 erlang Mash.new
 
 unless erlang.has_key?(:cookie)
+  cookie = ""
   24.times {
-    erlang[:cookie] <<
+    cookie <<
       (i = Kernel.rand(62);
        i += ((i < 10) ? 48 : ((i < 36) ? 55 : 61 ))).chr }
+  erlang[:cookie] = cookie
 end
