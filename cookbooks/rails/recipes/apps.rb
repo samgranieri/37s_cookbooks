68c07502a36b1caf22d1ce99e5b61fd9454cbe7b
Joshua Sierles <joshua@rasgueado.local>
2009-04-29 01:29:48 +0200

    use slice role to determine apache config filename

--- cookbooks/rails/recipes/apps.rb
+++ cookbooks/rails/recipes/apps.rb
@@ -22,7 +22,7 @@ if node[:active_applications]
   
   node[:active_applications].each do |app, conf|
     full_name = "#{app}_#{conf[:env]}"
-    filename = conf[:type] == "web" ? "#{conf[:env]}_web.conf" : "#{conf[:env]}.conf"
+    filename = node[:role] == "web" ? "#{conf[:env]}_web.conf" : "#{conf[:env]}.conf"
     path = "/u/apps/#{app}/current/config/apache/#{filename}"
     
     if modules = node[:applications][app][:apache_modules]
