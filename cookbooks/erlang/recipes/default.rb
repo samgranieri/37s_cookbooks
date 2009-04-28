a632dcb2454a282c58ec2f08785b45436842fed0
Mark Imbriaco <mark@imbriaco.com>
2009-04-28 00:52:21 -0400

    Install mochiweb and mysql.

--- cookbooks/erlang/recipes/default.rb
+++ cookbooks/erlang/recipes/default.rb
@@ -7,3 +7,13 @@ template "/home/app/.erlang.cookie" do
   group "app"
   mode 0600
 end
+
+bash "install erlang libraries" do
+  user "root"
+  cwd "/usr/lib/erlang/lib/erlang"
+
+  code <<-EOC
+  curl http://dist/packages/erlang/mochiweb-02.24.2009.tar.bz2 | tar xfj -
+  curl http://dist/packages/erlang/mysql-02.17.2009.tar.bz2 | tar xfj -
+  EOC
+end
