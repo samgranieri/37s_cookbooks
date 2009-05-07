4b676cdb499cbc9291f5aef629a90beb1d859159
Joshua Sierles <joshua@rasgueado.local>
2009-05-07 22:22:33 +0200

    restart associated service when updating template

--- cookbooks/runit/definitions/runit_service.rb
+++ cookbooks/runit/definitions/runit_service.rb
@@ -20,11 +20,17 @@ define :runit_service, :directory => nil, :only_if => false, :options => {} do
     action :create
   end
   
+  service params[:name] do
+    supports :restart => true, :status => true
+    action :nothing
+  end
+  
   template "#{sv_dir_name}/run" do
     mode 0755
     cookbook(params[:cookbook]) if params[:cookbook]
     source "sv-#{template_source}-run.erb"
-    variables(params[:options])
+    variables(params[:options]
+    notifies :restart, resources(:service => params[:name])
   end
   
   template "#{sv_dir_name}/log/run" do
@@ -32,6 +38,7 @@ define :runit_service, :directory => nil, :only_if => false, :options => {} do
     cookbook(params[:cookbook]) if params[:cookbook]
     source "sv-#{template_source}-log-run.erb"
     variables(params[:options])
+    notifies :restart, resources(:service => params[:name])
   end
   
   link "/etc/init.d/#{params[:name]}" do
@@ -41,12 +48,7 @@ define :runit_service, :directory => nil, :only_if => false, :options => {} do
   link "#{node[:runit_service_dir]}/#{params[:name]}" do 
     to "#{sv_dir_name}"
   end
-  
-  service params[:name] do
-    supports :restart => true, :status => true
-    action :nothing
-  end
-  
+    
   #execute "#{params[:name]}-down" do
   #  command "/etc/init.d/#{params[:name]} down"
   #  only_if do params[:only_if] end
