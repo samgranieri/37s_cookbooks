9b4c6ae945be84b223e0e520b0867fc5a22d985b
Joshua Sierles <joshua@rasgueado.local>
2009-05-07 21:29:18 +0200

    add cookbook source for runit service

--- cookbooks/memcached/recipes/instances.rb
+++ cookbooks/memcached/recipes/instances.rb
@@ -10,6 +10,7 @@ if node[:memcached][:instances]
   
     runit_service "memcached-#{name}" do
       template_name "memcached"
+      cookbook "memcached"
       options({:max_memory => node[:memcached][:max_memory],
                :port => node[:memcached][:port], :user => node[:memcached][:user],
                :max_connections => node[:memcached][:port]}.merge(instance))
