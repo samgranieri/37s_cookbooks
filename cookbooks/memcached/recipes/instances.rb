9175dc410a4dd60a2db1c2a587bfb9bdc612bef2
Joshua Sierles <joshua@rasgueado.local>
2009-05-07 22:17:42 +0200

    use string keys

--- cookbooks/memcached/recipes/instances.rb
+++ cookbooks/memcached/recipes/instances.rb
@@ -11,9 +11,9 @@ if node[:memcached][:instances]
     runit_service "memcached-#{name}" do
       template_name "memcached"
       cookbook "memcached"
-      options({:max_memory => node[:memcached][:max_memory],
-               :port => node[:memcached][:port], :user => node[:memcached][:user],
-               :max_connections => node[:memcached][:max_connections]}.merge(instance))
+      options({"max_memory" => node[:memcached][:max_memory],
+               "port" => node[:memcached][:port], "user" => node[:memcached][:user],
+               "max_connections" => node[:memcached][:max_connections]}.merge(instance))
     end
 
     service "memcached-#{name}" do
