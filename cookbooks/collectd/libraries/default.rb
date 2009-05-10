56cf84587886dc75ff521fa6bf227c321539b2e9
Mark Imbriaco <mark@imbriaco.com>
2009-05-09 22:04:35 -0400

    Make format method handle numbers and bools better.

--- cookbooks/collectd/libraries/default.rb
+++ cookbooks/collectd/libraries/default.rb
@@ -1,6 +1,6 @@
 
 def format_option(option)
-  return option if option.match(/^((true)|(false)|(\d+))$/)
+  return option if option.instance_of?(Fixnum) || option == true || option == false
   "\"#{option}\""
 end
 
