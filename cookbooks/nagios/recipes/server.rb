be01372221b13d7e76899f8c1fa20bdfe859eb80
Joshua Sierles <joshua@rasgueado.gateway.2wire.net>
2009-05-23 09:41:22 -0500

    make frequent service check a little more often

--- cookbooks/nagios/recipes/server.rb
+++ cookbooks/nagios/recipes/server.rb
@@ -125,7 +125,7 @@ nagios_template "frequent" do
   template_type "service"
 	use "default-service"
 	max_check_attempts    3
-  normal_check_interval 10
+  normal_check_interval 5
   retry_check_interval  20
 end
 
@@ -133,7 +133,7 @@ nagios_template "frequent-service-with-sms" do
   template_type "service"
 	use "default-service"
 	max_check_attempts    3
-  normal_check_interval 10
+  normal_check_interval 5
   retry_check_interval  20
   notification_interval 300
   contact_groups "admins, sysadmin-sms"
