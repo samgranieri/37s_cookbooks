edd86da150564ce91c329a12f017d2605bc3a45d
Joshua Sierles <joshua@rasgueado.local>
2009-06-23 17:48:00 +0200

    add apps to noc recipe

--- roles/noc.rb
+++ roles/noc.rb
@@ -1,5 +1,6 @@
 name "noc"
 description "Network opertions server"
-recipes "syslog::server", "nagios::server", "apt::mirror"
+recipes "basecamp", "highrise", "jobs", "backpack", "blogcabin", "bookstore", "campfire", "extra_extra", "forum", "help", "openbar", "queenbee", "writeboard",
+        "syslog::server", "nagios::server", "apt::mirror"
 default_attributes :active_groups => {:app => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}}
