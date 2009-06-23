88b3105f9575e97eb516fe245811c3ee281412e0
Joshua Sierles <joshua@rasgueado.local>
2009-06-23 16:21:41 +0200

    move syslog::client to app u

--- roles/app.rb
+++ roles/app.rb
@@ -1,5 +1,5 @@
 name "app"
 description "Basic Rails application server"
-recipes "rails::apps"
+recipes "rails::apps", "syslog::client"
 default_attributes :active_groups => {:dev => {:enabled => true}},
                    :active_sudo_groups => {:dev => {:enabled => true}}
