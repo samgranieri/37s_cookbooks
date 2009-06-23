52f8a1531f0fdc1dc3645574b4cd8e8ccc5f85e2
Joshua Sierles <joshua@rasgueado.local>
2009-06-23 17:50:21 +0200

    revert previous commit

--- roles/noc.rb
+++ roles/noc.rb
@@ -1,6 +1,5 @@
 name "noc"
 description "Network opertions server"
-recipes "basecamp", "highrise", "jobs", "backpack", "blogcabin", "bookstore", "campfire", "extra_extra", "forum", "help", "openbar", "queenbee", "writeboard",
-        "syslog::server", "nagios::server", "apt::mirror"
+recipes "syslog::server", "nagios::server", "apt::mirror"
 default_attributes :active_groups => {:app => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}}
