fbfc8f9e4e107246793878145a776d29a35369ec
Joshua Sierles <joshua@rasgueado.local>
2009-06-23 14:57:29 +0200

    remove nfs and and add apache listen ports

--- roles/basecamp.rb
+++ roles/basecamp.rb
@@ -1,12 +1,7 @@
 name "basecamp"
 description "Basecamp"
-default_attributes :active_applications =>  {"basecamp" =>  {"env" =>  "production"}},
+default_attributes :active_applications =>  {"basecamp" =>  {"env" => "production"}},
                    :syslog_files => {:completed => true, :thumbnails => true, :logsort => true, :haproxy => true, :memcache => true},
-                   :nfs_mounts => {
-                    "/u/nfs/file1/depot"  =>  {"device" => "172.28.1.66:/u/depot",  "owner"  => "app"},
-                    "/u/nfs/file2/depot"  =>  {"device" => "172.28.1.67:/u/depot",  "owner"  => "app"},
-                    "/u/nfs/file3/depot"  =>  {"device" => "172.28.1.70:/u/depot",  "owner"  => "app"},
-                    "/u/nfs/netapp/depot" =>  {"device" => "172.28.2.6:/vol/depot", "owner" => "app"}},
                     :gems => ['fast_xs', 'hpricot', 'aws-s3', 'ruby-prof', ['net-ssh', '1.1.4'], ['net-sftp', '1.1.1'], ['tzinfo', '0.3.9']],
                     :packages => ['imagemagick', 'elinks', 'zip'],
                     :symlinks => {
@@ -15,4 +10,5 @@ default_attributes :active_applications =>  {"basecamp" =>  {"env" =>  "producti
                       '/usr/local/bin/composite' => '/usr/bin/composite',
                       '/usr/local/bin/elinks' => '/usr/bin/elinks',
                       '/u/depot' => '/u/nfs/file1/depot'},
-                    :apache_modules => ["auth_token", "xsendfile", "rewrite"]
+                    :apache_modules => ["auth_token", "xsendfile", "rewrite"],
+                    :apache => {:listen_ports => []}
