52bb29d2558b1bc019af2c53f49ac10e5db76746
Joshua Sierles <joshua@rasgueado.local>
2009-06-24 21:13:07 +0200

    don't override the chef client enable attributes

--- cookbooks/chef/attributes/client.rb
+++ cookbooks/chef/attributes/client.rb
@@ -4,4 +4,4 @@ chef[:client_path] = `which chef-client`.chomp
 chef[:client_interval] = "300"
 chef[:client_splay] = "20"
 chef[:log_path] = "/var/log/chef/client.log"
-chef[:client_enable] = false
+chef[:client_enable] = false unless chef.has_key?(:client_enable)
