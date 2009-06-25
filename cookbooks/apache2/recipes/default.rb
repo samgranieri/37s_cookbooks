fba207784d8fe3e68fc971d80dd8ca87cb7bbacb
Joshua Sierles <joshua@rasgueado.local>
2009-06-25 14:19:56 +0200

    Revert "don't setup apache ports anymore"
    This reverts commit fb6df241ebeba7da809aa904b4da7bf21bd2bd42.

--- cookbooks/apache2/recipes/default.rb
+++ cookbooks/apache2/recipes/default.rb
@@ -79,8 +79,12 @@ template "apache2.conf" do
   notifies :reload, resources(:service => "apache2")
 end
 
-file "#{node[:apache][:dir]}/ports.conf" do
-  action :delete
+template "#{node[:apache][:dir]}/ports.conf" do
+  source "ports.conf.erb"
+  group "root"
+  owner "root"
+  variables :apache_listen_ports => node[:apache][:listen_ports]
+  mode 0644
 end
 
 template "#{node[:apache][:dir]}/mods-available/deflate.conf" do
