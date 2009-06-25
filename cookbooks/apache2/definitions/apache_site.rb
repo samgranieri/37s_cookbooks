43b5994239565b9b769b7c5ec382caf4b7acaa97
Joshua Sierles <joshua@rasgueado.local>
2009-06-25 17:57:18 +0200

    check sites-available for disablng apache sites

--- cookbooks/apache2/definitions/apache_site.rb
+++ cookbooks/apache2/definitions/apache_site.rb
@@ -19,7 +19,7 @@ define :apache_site, :enable => true do
     execute "a2dissite #{params[:name]}" do
       command "/usr/sbin/a2dissite #{params[:name]}"
       notifies :restart, resources(:service => "apache2")
-      only_if do File.symlink?("#{node[:apache][:dir]}/sites-enabled/#{params[:name]}") end
+      only_if do File.exists?("#{node[:apache][:dir]}/sites-available/#{params[:name]}") end
     end
   end
 end
