78488e01051c54db90f2f16cc437fb6b8db3b99d
Joshua Sierles <joshua@rasgueado.local>
2009-06-26 15:23:20 +0200

    move nfs mounts up one level

--- roles/highrise.rb
+++ roles/highrise.rb
@@ -2,13 +2,13 @@ name "highrise"
 description "Highrise production"
 recipes "nfs::client"
 default_attributes  :active_applications =>  {"highrise" =>  {"env" =>  "production"}},
+                    :nfs_mounts => {
+                    "/u/nfs/file1/depot"  =>  {"device" => "172.28.1.66:/u/depot",  "owner"  => "app"},
+                    "/u/nfs/file2/depot"  =>  {"device" => "172.28.1.67:/u/depot",  "owner"  => "app"},
+                    "/u/nfs/file3/depot"  =>  {"device" => "172.28.1.70:/u/depot",  "owner"  => "app"},
+                    "/u/nfs/netapp/depot" =>  {"device" => "172.28.2.6:/vol/depot", "owner" => "app"}},
                     :applications => {
                       :highrise => {
-                        :nfs_mounts => {
-                        "/u/nfs/file1/depot"  =>  {"device" => "172.28.1.66:/u/depot",  "owner"  => "app"},
-                        "/u/nfs/file2/depot"  =>  {"device" => "172.28.1.67:/u/depot",  "owner"  => "app"},
-                        "/u/nfs/file3/depot"  =>  {"device" => "172.28.1.70:/u/depot",  "owner"  => "app"},
-                        "/u/nfs/netapp/depot" =>  {"device" => "172.28.2.6:/vol/depot", "owner" => "app"}},
                         :syslog_files => {:haproxy => true},
                         :gems => ['fast_xs','hpricot'],
                         :packages => ['imagemagick', 'zip'],
