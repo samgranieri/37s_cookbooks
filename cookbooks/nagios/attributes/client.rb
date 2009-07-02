a0df827731640831e76d3238114a8417b0197d60
Joshua Sierles <joshua@rasgueado.local>
2009-07-02 16:01:24 +0200

    add default haproxy check values

--- cookbooks/nagios/attributes/client.rb
+++ cookbooks/nagios/attributes/client.rb
@@ -3,6 +3,7 @@ nagios Mash.new unless attribute?("nagios")
 nagios[:checks] = Mash.new unless nagios.has_key?(:checks)
 nagios[:checks][:load] = Mash.new unless nagios[:checks].has_key?(:load)
 nagios[:checks][:memory] = Mash.new unless nagios[:checks].has_key?(:memory)
+nagios[:checks][:haproxy_queue] = Mash.new unless nagios[:checks].has_key?(:haproxy_queue)
 
 nagios[:checks][:memory][:critical] = 150 unless nagios[:checks][:memory].has_key?(:critical)
 nagios[:checks][:memory][:warning] = 250 unless nagios[:checks][:memory].has_key?(:warning)
