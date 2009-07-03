13a480b9709e46b4958a46320e6fb67280b6cc0e
Joshua Sierles <joshua@rasgueado.local>
2009-07-03 14:39:50 +0200

    move haproxy checks to proxy boxes

--- roles/bc-web.rb
+++ roles/bc-web.rb
@@ -1,6 +1,6 @@
 name "bc-web"
 description "Basecamp web server"
-recipes "syslog::client", "haproxy"
+recipes "syslog::client"
 default_attributes :active_groups => {:app => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}},
                    :apache => {
