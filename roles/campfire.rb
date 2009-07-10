b1920ca758a181a9b3d3fcf333e38de996477fc2
Joshua Sierles <joshua@rasgueado.local>
2009-07-10 20:14:29 +0200

    add campfire apache ports

--- roles/campfire.rb
+++ roles/campfire.rb
@@ -20,3 +20,4 @@ default_attributes :active_applications =>  {"campfire" =>  {"env" =>  "producti
                         :apache_modules => ["auth_token", "xsendfile", "fastcgi", "rewrite"]
                       }
                     }
+override_attributes :apache => {:ports => [80,443]}
