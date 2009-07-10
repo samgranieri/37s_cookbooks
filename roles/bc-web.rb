1a7aefb0936424bd68b86bc074c7e4a7130fee86
Joshua Sierles <joshua@rasgueado.local>
2009-07-10 11:09:10 +0200

    move apache attributes to override for bc web

--- roles/bc-web.rb
+++ roles/bc-web.rb
@@ -2,16 +2,17 @@ name "bc-web"
 description "Basecamp web server"
 recipes "syslog::client", "rails::apps"
 default_attributes :active_groups => {:app => {:enabled => true}},
-                   :active_sudo_groups => {:app => {:enabled => true}},
-                   :web_only => true,
-                   :apache => {
-                     :listen_ports => [],
-                     :worker => {:startservers => 30,
+                   :active_sudo_groups => {:app => {:enabled => true}}
+
+override_attributes :web_only => true,
+                    :apache => {
+                      :listen_ports => [],
+                      :worker => {:startservers => 30,
                                  :maxclients => 1536,
                                  :serverlimit => 64,
                                  :minsparethreads => 250,
                                  :maxsparethreads => 400,
                                  :threadsperchild => 24,
                                  :maxrequestsperchild => 0
-                                 }
+                       }
                     }
