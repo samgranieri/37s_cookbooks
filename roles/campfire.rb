6375cb8ff7b35de76e66ee3344566509873255a4
Joshua Sierles <joshua@rasgueado.local>
2009-07-15 09:05:24 +0200

    move CF nfs mounts to top level

--- roles/campfire.rb
+++ roles/campfire.rb
@@ -1,14 +1,14 @@
 name "campfire"
 description "Campfire production"
 default_attributes :active_applications =>  {"campfire" =>  {"env" =>  "production"}},
+                  :nfs_mounts => {
+                   "/u/nfs/file1/depot"  =>  {"device" => "172.28.1.66:/u/depot",  "owner"  => "app"},
+                   "/u/nfs/file2/depot"  =>  {"device" => "172.28.1.67:/u/depot",  "owner"  => "app"},
+                   "/u/nfs/file3/depot"  =>  {"device" => "172.28.1.70:/u/depot",  "owner"  => "app"},
+                   "/u/nfs/netapp/depot" =>  {"device" => "172.28.2.6:/vol/depot", "owner" => "app"}},
                    :applications => {
                      :campfire => {
                        :syslog_files => {:haproxy => true},
-                       :nfs_mounts => {
-                        "/u/nfs/file1/depot"  =>  {"device" => "172.28.1.66:/u/depot",  "owner"  => "app"},
-                        "/u/nfs/file2/depot"  =>  {"device" => "172.28.1.67:/u/depot",  "owner"  => "app"},
-                        "/u/nfs/file3/depot"  =>  {"device" => "172.28.1.70:/u/depot",  "owner"  => "app"},
-                        "/u/nfs/netapp/depot" =>  {"device" => "172.28.2.6:/vol/depot", "owner" => "app"}},
                         :gems => ['fast_xs', 'hpricot', 'mime-types', ['tzinfo', '0.3.9'], "xml-simple"],
                         :packages => ['imagemagick', 'libfcgi-dev'],
                         :symlinks => {
