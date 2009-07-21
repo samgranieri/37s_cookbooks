faa4bdd882c1ca14cfe4febe3374cf6b73276a6b
Joshua Sierles <joshua@rasgueado.local>
2009-07-21 18:28:53 +0200

    move mail queue checks to mail-03

--- roles/mail-master.rb
+++ roles/mail-master.rb
@@ -4,12 +4,12 @@ description 'Mail master server'
 override_attributes :cron => {
     :jobs => {
       :hr_dropbox_worker => {
-        :command => '/usr/local/bin/ruby /u/apps/highrise/current/script/dropbox_worker 50 > /dev/null',
+        :command => '/usr/local/bin/ruby /u/apps/highrise/current/script/dropbox_worker 50',
         :minute => '*/5',
         :user => 'app'
       },
       :bp_email_worker => {
-        :command => '/usr/local/bin/ruby /u/apps/backpack/current/script/email_worker 50 > /dev/null',
+        :command => '/usr/local/bin/ruby /u/apps/backpack/current/script/email_worker 50',
         :minute => "*/5",
         :user => 'app'
       }
