25e5b61214f03697c40e791ddfdf807830cf62f8
Mark Imbriaco <mark@imbriaco.com>
2009-07-23 17:28:05 -0400

    Fix WAR file path.

--- cookbooks/solr/attributes/solr.rb
+++ cookbooks/solr/attributes/solr.rb
@@ -5,4 +5,4 @@ solr[:data_device] = nil unless solr.has_key?(:data_device)
 
 solr[:war] = Mash.new unless solr.has_key?(:war)
 solr[:war][:file] = "apache-solr-1.3.0.war" unless solr[:war].has_key?(:file)
-solr[:war][:url] = "http://dist/war" unless solr[:war].has_key?(:url)
+solr[:war][:url] = "http://dist/solr" unless solr[:war].has_key?(:url)
