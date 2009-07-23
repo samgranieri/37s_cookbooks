d1229e5ba81a5603dbd531efd73691200784bdbf
Joshua Sierles <joshua@rasgueado.local>
2009-07-23 13:56:14 +0200

    use override_attributes instead of default

--- roles/base.rb
+++ roles/base.rb
@@ -3,7 +3,7 @@ description "Base role that applies to all servers"
 recipes "ruby-shadow", "hosts", "dns::client", "chef::client", "nagios::client", "rubygems::client", "timezone",
         "git", "postfix", "ssh::server", "users", "sudo", "sysadmin", "mysql::client", "chef::client", "collectd"
 
-default_attributes :active_groups => {:admin => {:enabled => true}},
+override_attributes :active_groups => {:admin => {:enabled => true}},
                   :active_sudo_groups => {:admin => {:enabled => true}},
                   :sysadmin_email => "sysadmins@37signals.com",
                   :sysadmin_sms_email => "sysadmins@37signals.com",
