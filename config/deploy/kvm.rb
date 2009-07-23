cf1f344e4d0923457b464267bab9c267b495e931
Joshua Sierles <joshua@rasgueado.local>
2009-07-23 12:30:31 +0200

    update http checks for SSL and following redirects

---
+++ config/deploy/kvm.rb
@@ -0,0 +1,9 @@
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")] 
+set :user, ENV['USER']
+set :use_sudo, false
+set :gateway, "#{ENV['USER']}@dev.37signals.com"
+role :kvm, "xen11", "xen12", "kvm01", "kvm02", "kvm03", "kvm04", "kvm05", "kvm06", "kvm07", "kvm08", "kvm09", "kvm10"
+
+task :find do
+  run "kvmtool list | grep #{ENV['VM']}"
+end
