3ec4011be395e6908cf0afb3236ff4d9db82c0e1
Joshua Sierles <joshua@rasgueado.local>
2009-07-24 12:03:52 +0200

    add app dependencies recipe

--- roles/hr-cron.rb
+++ roles/hr-cron.rb
@@ -1,5 +1,5 @@
 name "hr-cron"
 description "HR Cron/utility server"
-recipes "cron", "nfs::client", "syslog::client"
+recipes "cron", "nfs::client", "syslog::client", "rails::app_dependencies"
 default_attributes :active_groups => {:app => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}}
