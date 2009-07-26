33f740aa405db4bf19d956522464965aaeb04d3f
Mark Imbriaco <mark@imbriaco.com>
2009-07-25 21:53:50 -0400

    Add Rails app dependencies to Solr servers to build the basic /u/apps structure out.

--- roles/solr.rb
+++ roles/solr.rb
@@ -1,5 +1,6 @@
 name "solr"
 description "Solr search server"
-recipes "solr"
+recipes "solr", "rails::app_dependencies"
+
 default_attributes :active_groups => {:app => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}}
