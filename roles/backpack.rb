01173a4fce5c1a57e209887c3cf7b1f79b22047f
Mark Imbriaco <mark@imbriaco.com>
2009-07-27 17:34:36 -0400

    Remove netapp depot mount.

--- roles/backpack.rb
+++ roles/backpack.rb
@@ -18,8 +18,7 @@ default_attributes  :active_applications =>  {"backpack" =>  {"env" =>  "product
                     :nfs_mounts => {
                       "/u/nfs/file1/depot"  =>  {"device" => "172.28.1.66:/u/depot",  "owner"  => "app"},
                       "/u/nfs/file2/depot"  =>  {"device" => "172.28.1.67:/u/depot",  "owner"  => "app"},
-                      "/u/nfs/file3/depot"  =>  {"device" => "172.28.1.70:/u/depot",  "owner"  => "app"},
-                      "/u/nfs/netapp/depot" =>  {"device" => "172.28.2.6:/vol/depot", "owner" => "app"}
+                      "/u/nfs/file3/depot"  =>  {"device" => "172.28.1.70:/u/depot",  "owner"  => "app"}
                     }
                     
 override_attributes :apache => {:listen_ports => [80,443]},
--- roles/mail.rb
+++ roles/mail.rb
@@ -13,8 +13,7 @@ override_attributes :active_groups => {:app => {:enabled => true}},
   :nfs_mounts => {
     "/u/nfs/file1/depot"  =>  {"device" => "172.28.1.66:/u/depot",  "owner"  => "app"},
     "/u/nfs/file2/depot"  =>  {"device" => "172.28.1.67:/u/depot",  "owner"  => "app"},
-    "/u/nfs/file3/depot"  =>  {"device" => "172.28.1.70:/u/depot",  "owner"  => "app"},
-    "/u/nfs/netapp/depot" =>  {"device" => "172.28.2.6:/vol/depot", "owner" => "app"}
+    "/u/nfs/file3/depot"  =>  {"device" => "172.28.1.70:/u/depot",  "owner"  => "app"}
   },
   :cron => {
     :jobs => {
