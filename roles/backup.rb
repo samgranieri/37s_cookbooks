a4c294536113bc61294c8f836cc0c0c6504bfad5
Mark Imbriaco <mark@imbriaco.com>
2009-07-27 16:15:48 -0400

    Add NFS serving to the Backup role.

--- roles/backup.rb
+++ roles/backup.rb
@@ -1,5 +1,5 @@
 name "backup"
 description "Backup Server"
-recipes "backup::server"
+recipes "backup::server", "nfs::server"
 
 default_attributes :active_groups => {:storage => {:enabled => true}}
