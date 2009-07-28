64d20ecc2b7682a13fe4524cccfddaebaee6c281
Joshua Sierles <joshua@rasgueado.local>
2009-07-28 22:28:14 +0200

    add memcache instances to memcache role

--- roles/memcache.rb
+++ roles/memcache.rb
@@ -1,3 +1,11 @@
 name "memcache"
 description "Memcache server"
 recipes "memcached::instances"
+default_attributes :memcached => {
+                     :instances => {  
+                       :basecamp => {"max_memory" => "256", "port" => "11211"},
+                       :backpack => {"max_memory" => "1024", "port" => "11212"},
+                       :shared   => {"max_memory" => "256", "port" => "11213"},
+                       :highrise => {"max_memory" => "1024", "port" => "11214"}
+                     }
+                   }
