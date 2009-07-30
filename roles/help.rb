fc021797e4657042b49331dc65ddbd85f8522f17
Joshua Sierles <joshua@rasgueado.local>
2009-07-30 14:49:11 -0700

    rename attribute

--- roles/help.rb
+++ roles/help.rb
@@ -2,7 +2,7 @@ name "help"
 description "Support help app"
 default_attributes :active_applications =>  {"help" =>  {"env" =>  "production"}},
                    :applications => {
-                                       :bookstore => {
-                                        :gems => ['fast_xs', "RedCloth"]
-                                       }
-                                     }
+                     :help => {
+                      :gems => ['fast_xs', "RedCloth"]
+                     }
+                   }
