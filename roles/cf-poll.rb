221a534d6f2773c2efdf9b06c676b5a084f0ffb4
Joshua Sierles <joshua@rasgueado.local>
2009-07-30 10:35:04 -0700

    add cf-poll role

---
+++ roles/cf-poll.rb
@@ -0,0 +1,18 @@
+name "cf-poll"
+description "CF Solr search server"
+recipes "erlang", "rails::app_dependencies"
+
+default_attributes :erlang => {
+                     :applications => [
+                        { 
+                         "name" => "campfire_poller",
+                         "description" => "Campfire poller server",
+                         "base_dir" => "/u/apps/campfire_poller",
+                         "supervisor_name" => "poller",
+                         "erlang_args" => "+K true +A 32 -smp enable",
+                         "fullsweep_after" => "0"
+                       }
+                     ],
+                     "cookie" => "HpYSdvIxrd34QGFIJMFv"
+                   },
+                   "active_applications" => { "campfire" => { "env" => "production" }}
