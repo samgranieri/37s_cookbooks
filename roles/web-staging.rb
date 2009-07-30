fa21b91a897cc31c43c5f2beed4a0dd272cc40eb
Joshua Sierles <joshua@rasgueado.local>
2009-07-30 11:18:59 -0700

    add web-staging and mmm-mon role

---
+++ roles/web-staging.rb
@@ -0,0 +1,13 @@
+name "web-staging"
+description "Staging slice for smaller sites/apps"
+recipes "mysql::server", "php5::sites", "rails::apps"
+override_attributes "active_applications" => {"blogcabin" => {"env" => "staging"},
+                 "bookstore" => {"env" => "staging"},
+                 "writeboard" => {"env" => "staging"},
+                 "jobs" => {"env" => "staging"},
+                 "help" => {"env" => "staging"},
+                 "extra_extra" => {"env" => "staging"}},
+ "active_sites" => {"37signals" => {"env" => "staging"},
+                  "backpackit" => {"env" => "staging"},
+                  "highrisehq" => {"env" => "staging"}}
+  
