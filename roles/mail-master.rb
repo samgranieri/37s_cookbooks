202831e7648399d15b2a2c081c6e31d85668f0cb
Mark Imbriaco <mark@imbriaco.com>
2009-08-13 09:56:53 -0400

    Purge Highrise email logs older than 5 days old.

--- roles/mail-master.rb
+++ roles/mail-master.rb
@@ -8,6 +8,11 @@ override_attributes :cron => {
         :minute => '*/5',
         :user => 'app'
       },
+      :hr_email_log_purger => {
+        :command => 'find /u/apps/highrise/shared/log/emails/2009/* -maxdepth 1 -type d -mtime +5 -exec rm -rf {} \;',
+        :hour => "0",
+        :user => 'app',
+      },
       :bp_email_worker => {
         :command => '/usr/local/bin/ruby /u/apps/backpack/current/script/email_worker 50',
         :minute => "*/5",
