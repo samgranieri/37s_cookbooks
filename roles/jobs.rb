2a1449140d8fad94f3237f85e094ea2f82ca3533
Mark Imbriaco <mark@imbriaco.com>
2009-08-17 09:45:26 -0400

    Add the app group to the active groups on the jobs server.

--- roles/jobs.rb
+++ roles/jobs.rb
@@ -1,6 +1,7 @@
 name "jobs"
 description "Jobs"
 default_attributes :active_applications =>  {"jobs" =>  {"env" =>  "production"}},
+                   :active_groups => { :app => { :enabled => true }},
                    :applications => {
                      :jobs => {
                        :gems => ['fast_xs', 'RedCloth', 'mime-types', "xml-simple"]                       
