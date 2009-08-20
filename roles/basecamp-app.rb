7ee71ffcdfd8e6b73b3431999b171759e7b253b6
Mark Imbriaco <mark@imbriaco.com>
2009-08-20 14:53:14 -0400

    Add cron job to remove RackMultipart.* files so we don't fill up the drive.

--- roles/basecamp-app.rb
+++ roles/basecamp-app.rb
@@ -1,4 +1,14 @@
 name "basecamp-app"
 description "Basecamp Application Server"
+recipes "cron"
 
-override_attributes :apache => { :deflate => { :disable => true, :mime_types => [] }}
+override_attributes :apache => { :deflate => { :disable => true, :mime_types => [] }},
+                    :cron => {
+                      :jobs => {
+                        :rack_multipart_cleaner => {
+                          :command => '/usr/bin/find /tmp -name RackMultipart.\* -mmin +15 -delete',
+                          :minute => '*/15',
+                          :user => 'root'
+                        }
+                      }
+                    }
