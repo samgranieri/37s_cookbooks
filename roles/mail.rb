b4fe66f38b86752d8adb49eedb33c1a93fa6ba64
Mark Imbriaco <mark@imbriaco.com>
2009-08-21 13:26:43 -0400

    Fix basecamp@... rule so it doesn't gobble any address that so much as begins with basecamp.

--- roles/mail.rb
+++ roles/mail.rb
@@ -65,7 +65,7 @@ override_attributes :active_groups => {:app => {:enabled => true}},
     
       '/^([^@]+\.)?(updatelog|clientsection|seework|grouphub|projectpath|basecamphq)\.com$/' => 'VIRTUAL',
       '/^P\d+-reply@(([^@])+\.)basecamphq\.com$/' => 'basecamp@localhost',
-      '/^(basecamp)|([Uu]\d+[PpIiMm]\d+-reply)@([^\.]+\.)(updatelog|clientsection|seework|grouphub|projectpath|basecamphq)\.com$/' => 'basecamp@localhost',
+      '/^((basecamp)|([Uu]\d+[PpIiMm]\d+-reply))@([^\.]+\.)(updatelog|clientsection|seework|grouphub|projectpath|basecamphq)\.com$/' => 'basecamp@localhost',
       '/^do-not-reply(.+)?@([^\.]+\.)*(updatelog|clientsection|seework|grouphub|projectpath|basecamphq)\.com$/' => 'devnull',
       '/^notifications@basecamphq\.com$/' => 'devnull',
       '/^postmaster@(updatelog|clientsection|seework|grouphub|projectpath|basecamphq)\.com$/' => 'postmaster@37signals.com',
