b6967a4c030fe0be21a39c6fe46fba10dd44929e
Mark Imbriaco <mark@imbriaco.com>
2009-08-24 17:19:43 -0400

    Add JRuby to backup role.

--- roles/backup.rb
+++ roles/backup.rb
@@ -1,5 +1,5 @@
 name "backup"
 description "Backup Server"
-recipes "backup::server", "nfs::server"
+recipes "backup::server", "nfs::server", "jruby"
 
 default_attributes :active_groups => {:storage => {:enabled => true}}
