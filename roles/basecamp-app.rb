c7f1a4cedc0e7d574f35cc8600b68940bb02c576
Mark Imbriaco <mark@imbriaco.com>
2009-08-24 22:19:51 -0400

    Rack tmp files were moved to /u/apps/basecamp/shared/tmp

--- roles/basecamp-app.rb
+++ roles/basecamp-app.rb
@@ -6,7 +6,7 @@ override_attributes :apache => { :deflate => { :disable => true, :mime_types => 
                     :cron => {
                       :jobs => {
                         :rack_multipart_cleaner => {
-                          :command => '/usr/bin/find /tmp -name RackMultipart.\* -mmin +15 -delete',
+                          :command => '/usr/bin/find /u/apps/basecamp/shared/tmp -name RackMultipart.\* -mmin +15 -delete',
                           :minute => '*/15',
                           :user => 'root'
                         }
