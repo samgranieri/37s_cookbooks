3dae99ddad8a64b9c8d74e95bd418db9c21902d7
Joshua Sierles <joshua@rasgueado.local>
2009-08-26 08:50:01 +0200

    staging often uses haproxy

--- roles/staging.rb
+++ roles/staging.rb
@@ -1,3 +1,3 @@
 name "staging"
 description "Staging server"
-recipes "rails::apps", "syslog::client", "nfs::client", "mysql::server", "nfs::client", "erlang"
+recipes "rails::apps", "syslog::client", "nfs::client", "mysql::server", "nfs::client", "erlang", "haproxy"
