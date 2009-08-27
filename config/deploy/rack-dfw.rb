fa0314a0190cbfeaa94de7cf22d338968b871806
Joshua Sierles <joshua@rasgueado.local>
2009-08-27 15:22:34 +0200

    deploy via gateway

--- config/deploy/rack-dfw.rb
+++ config/deploy/rack-dfw.rb
@@ -1,5 +1,5 @@
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")] 
 set :user, ENV['USER']
 set :use_sudo, false
-set :gateway, "#{ENV['USER']}@dev.37signals.com"
+set :gateway, "#{ENV['USER']}@gateway.37signals.com"
 role :app, "noc2"
