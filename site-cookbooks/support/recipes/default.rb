78dd2d993504c7f0374c8dac73eb34fe8b2a63ac
Joshua Sierles <joshua@rasgueado.local>
2009-08-27 13:01:00 +0200

    create a directory for support related files

--- site-cookbooks/support/recipes/default.rb
+++ site-cookbooks/support/recipes/default.rb
@@ -1,9 +1,23 @@
 # support tools
-remote_file "/usr/local/bin/support_check.rb" do
+
+directory "/u/support" do
+  owner "app"
+  group "app"
+  mode 0755
+end
+
+directory "/u/support/bin" do
+  owner "app"
+  group "app"
+  mode 0755
+end
+
+remote_file "/u/support/bin/gmail_queue_check.rb" do
   source "support_check.rb"
   mode 0755
 end
 
 cron "support email queue check" do
-  command "/usr/local/bin/support_check.rb"
+  command "/u/support/bin/gmail_queue_check.rb"
+  minute "*/10"
 end
