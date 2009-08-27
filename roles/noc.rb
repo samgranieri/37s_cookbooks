c34cd6786b2a8ccc1ac4eb92cda6dd304485c44d
Joshua Sierles <joshua@rasgueado.local>
2009-08-27 11:36:57 +0200

    add support check cron job

--- roles/noc.rb
+++ roles/noc.rb
@@ -1,6 +1,6 @@
 name "noc"
 description "Network opertions server"
-recipes "syslog::server", "nagios::server", "apt::mirror"
+recipes "syslog::server", "nagios::server", "apt::mirror", "support"
 default_attributes :active_groups => {:app => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}},
                    :chef => {:validation_token => "372fa8ac3d0f87395f986314897b835a"},
--- site-cookbooks/support/recipes/default.rb
+++ site-cookbooks/support/recipes/default.rb
@@ -3,3 +3,7 @@ remote_file "/usr/local/bin/support_check.rb" do
   source "support_check.rb"
   mode 0755
 end
+
+cron "support email queue check" do
+  command "/usr/local/bin/support_check.rb"
+end
