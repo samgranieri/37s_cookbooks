c1e30f33b160edbbebe143f14b57dee6db8e6995
Joshua Sierles <joshua@rasgueado.local>
2009-08-27 20:41:35 +0200

    add git mirror update cron job

--- roles/git.rb
+++ roles/git.rb
@@ -1,5 +1,14 @@
 name "git"
 description "Git source server"
-recipes "git::server"
+recipes "git::server", "cron"
 default_attributes :active_groups => {:app => {:enabled => true}, :git => {:enabled => true}},
-                   :active_sudo_groups => {:app => {:enabled => true}}
+                   :active_sudo_groups => {:app => {:enabled => true}},
+                   :cron => {
+                     :jobs => {
+                       :git_mirrors => {
+                         :minute => "*/2",
+                         :command => "/u/git/mirrors/update_all_mirrors.sh > /dev/null",
+                         :user => "git"
+                       }
+                     }
+                   }
