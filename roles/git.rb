ba140ed7f150c942ee589922a53ea2af855e48fa
Mark Imbriaco <mark@imbriaco.com>
2009-08-27 09:15:22 -0400

    Add the git group to the git role.

--- roles/git.rb
+++ roles/git.rb
@@ -1,5 +1,5 @@
 name "git"
 description "Git source server"
 recipes "git::server"
-default_attributes :active_groups => {:app => {:enabled => true}},
+default_attributes :active_groups => {:app => {:enabled => true}, :git => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}}
