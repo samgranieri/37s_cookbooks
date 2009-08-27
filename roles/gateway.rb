ed9a86af7063c54b695a7c8f4d692403d3495db2
Joshua Sierles <joshua@rasgueado.local>
2009-08-27 17:10:23 +0200

    add app group to gateway

--- roles/gateway.rb
+++ roles/gateway.rb
@@ -1,2 +1,4 @@
 name "gateway"
 description "SSH Gateway Server"
+default_attributes :active_groups => {:app => {:enabled => true}},
+                   :active_sudo_groups => {:app => {:enabled => true}}
