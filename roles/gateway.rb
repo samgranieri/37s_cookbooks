f70972e5e0a61974cc2bec8fda01a85e3095f9c2
Joshua Sierles <joshua@rasgueado.local>
2009-08-27 17:47:41 +0200

    add site users to gateway

--- roles/gateway.rb
+++ roles/gateway.rb
@@ -1,4 +1,3 @@
 name "gateway"
 description "SSH Gateway Server"
-default_attributes :active_groups => {:app => {:enabled => true}},
-                   :active_sudo_groups => {:app => {:enabled => true}}
+default_attributes :active_groups => {:app => {:enabled => true}, :site => {:enabled => true}}
