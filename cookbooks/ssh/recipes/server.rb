34cd1f2575d504134895c632ee58a898f0109d4d
Joshua Sierles <joshua@rasgueado.local>
2009-08-28 13:53:46 +0200

    add ssh_config template and a known_hosts file

--- cookbooks/ssh/recipes/server.rb
+++ cookbooks/ssh/recipes/server.rb
@@ -3,6 +3,18 @@ service "ssh" do
   action :enable
 end
 
+file "/etc/ssh/known_hosts" do
+  source "known_hosts"
+  mode 0644
+end
+
+template "/etc/ssh/ssh_config" do
+  source "ssh_config.erb"
+  mode 0644
+  owner "root"
+  group "root"
+end
+
 template "/etc/ssh/sshd_config" do
   source "sshd_config.erb"
   mode 0644
@@ -10,6 +22,7 @@ template "/etc/ssh/sshd_config" do
   group "root"
   notifies :restart, resources(:service => "ssh")
 end
+
 
 service "ssh" do
   action :start
