349547ac16bb5b075bad8aa7e3ab76d7bec128ff
Mark Imbriaco <mark@imbriaco.com>
2009-09-01 12:12:59 -0400

    Need to add the cron recipe.

--- roles/noc.rb
+++ roles/noc.rb
@@ -1,6 +1,7 @@
 name "noc"
 description "Network opertions server"
-recipes "syslog::server", "nagios::server", "apt::mirror", "support"
+recipes "syslog::server", "nagios::server", "apt::mirror", "support", "cron"
+
 default_attributes :active_groups => {:app => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}},
                    :chef => {:validation_token => "372fa8ac3d0f87395f986314897b835a"},
