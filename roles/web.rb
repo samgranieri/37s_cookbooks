5111064e74da6ccd2e7153372932dd56ad6aa92a
Joshua Sierles <joshua@rasgueado.local>
2009-09-01 20:05:06 +0200

    reenable tcp window resizing on web slices since they don't send email

--- roles/web.rb
+++ roles/web.rb
@@ -4,3 +4,9 @@ recipes "rails::web", "syslog::client", "nfs::client"
 
 default_attributes :active_groups => {:app => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}}
+                   
+override_attributes :sysctl => {
+                     :settings => {
+                       "net.ipv4.tcp_window_scaling" => "1"
+                     }
+                   }
