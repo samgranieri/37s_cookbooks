fe3ba2d38d1f6d6e2bbd3d899d4d9f5efd8740b5
Joshua Sierles <joshua@rasgueado.local>
2009-09-01 17:52:50 +0200

    check for existence of nameserver attribute before updating resolv.con

--- cookbooks/dns/recipes/client.rb
+++ cookbooks/dns/recipes/client.rb
@@ -1,5 +1,7 @@
-template "/etc/resolv.conf" do
-  source "resolv.conf.erb"
-  variables ({:domain => node[:domain], :nameservers => node[:nameservers], :search => node[:domain]})
-  not_if { node[:nameservers].include? node[:ipaddress] }
+if node[:nameservers]
+  template "/etc/resolv.conf" do
+    source "resolv.conf.erb"
+    variables ({:domain => node[:domain], :nameservers => node[:nameservers], :search => node[:domain]})
+    not_if { node[:nameservers].include? node[:ipaddress] }
+  end
 end
