629688e73383d9baeeb15b47ea73d275c7389e44
Joshua Sierles <joshua@rasgueado.local>
2009-09-02 22:12:53 +0200

    add app role to staging

--- roles/staging.rb
+++ roles/staging.rb
@@ -1,3 +1,6 @@
 name "staging"
 description "Staging server"
 recipes "rails::apps", "syslog::client", "nfs::client", "mysql::server", "nfs::client", "erlang", "haproxy", "memcached"
+
+default_attributes :active_groups => {:app => {:enabled => true}},
+                   :active_sudo_groups => {:app => {:enabled => true}}
