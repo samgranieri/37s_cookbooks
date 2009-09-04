b5069ae59ce7024dae3d178adf6ccd1f554ccf2f
Joshua Sierles <joshua@rasgueado.local>
2009-09-04 19:32:36 +0200

    add rsnaphot server recipe to bulk

--- roles/backup.rb
+++ roles/backup.rb
@@ -1,6 +1,6 @@
 name "backup"
 description "Backup Server"
-recipes "backup::server", "nfs::server", "java", "gnupg"
+recipes "backup::server", "rsnapshot::server", "nfs::server", "java", "gnupg"
 
 default_attributes :active_groups => {:storage => {:enabled => true}},
                    :rsnapshot =>  {
