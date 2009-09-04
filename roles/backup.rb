04af29e0f695c07342931abbacea4bf195efa189
Joshua Sierles <joshua@rasgueado.local>
2009-09-04 19:49:23 +0200

    add QB log backups

--- roles/backup.rb
+++ roles/backup.rb
@@ -5,6 +5,8 @@ recipes "backup::server", "rsnapshot::server", "nfs::server", "java", "gnupg"
 default_attributes :active_groups => {:storage => {:enabled => true}},
                    :rsnapshot =>  {
                      :backups => {
-                       "storage@git:/u/git" => {:target => "git"}
+                       "storage@git:/u/git/" => {:target => "git"},
+                       "storage@qb-01:/u/apps/queenbee/shared/log/" => {:target => "qb-01"},
+                       "storage@qb-02:/u/apps/queenbee/shared/log/" => {:target => "qb-02"}
                      }
                    }
