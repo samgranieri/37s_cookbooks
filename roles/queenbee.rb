1d69506551854216eb8aac81e9613b82c8d8488a
Joshua Sierles <joshua@rasgueado.local>
2009-09-04 20:03:12 +0200

    add storage and backup client to queenbee role

--- roles/queenbee.rb
+++ roles/queenbee.rb
@@ -1,8 +1,11 @@
 name "queenbee"
 description "Queenbee"
+recipes "backup::client"
 default_attributes :active_applications =>  {"queenbee" =>  {"env" =>  "production"}},
                    :applications => {
                      :queenbee => {
                        :gems => ['fast_xs']
                      }
-                   }
+                   },
+                   :active_groups => {:storage => {:enabled => true}}
+                   
