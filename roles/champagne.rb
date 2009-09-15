94b5cf437ed0d989aee5006d236da01173320f52
Joshua Sierles <joshua@rasgueado.local>
2009-09-15 18:29:03 +0200

    lowercase role

--- roles/champagne.rb
+++ roles/champagne.rb
@@ -1,4 +1,4 @@
-name "Champagne"
+name "champagne"
 description "Champagne"
 default_attributes :active_applications =>  {"champagne" =>  {"env" =>  "production"}},
                    :applications => {
