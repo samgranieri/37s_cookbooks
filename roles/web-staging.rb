1daf78cf22116861d71a790144a0ab18637d6ffa
Joshua Sierles <joshua@rasgueado.local>
2009-09-15 18:23:01 +0200

    fix app hash

--- roles/web-staging.rb
+++ roles/web-staging.rb
@@ -5,8 +5,8 @@ default_attributes "active_applications" => {"blogcabin" => {"env" => "staging"}
                  "bookstore" => {"env" => "staging"},
                  "jobs" => {"env" => "staging"},
                  "help" => {"env" => "staging"},
-                 "extra_extra" => {"env" => "staging"}},
-                 "champagne" => {"env" => "staging"}}
+                 "extra_extra" => {"env" => "staging"},
+                 "champagne" => {"env" => "staging"}},
  "active_sites" => {"37signals" => {"env" => "staging"},
                   "backpackit" => {"env" => "staging"},
                   "highrisehq" => {"env" => "staging"}}
