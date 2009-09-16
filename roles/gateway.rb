654569d30dbfe4ac2b9f1605765f44daae366519
Joshua Sierles <joshua@rasgueado.local>
2009-09-16 18:02:24 +0200

    add contractors to gateway

--- roles/gateway.rb
+++ roles/gateway.rb
@@ -1,3 +1,3 @@
 name "gateway"
 description "SSH Gateway Server"
-default_attributes :active_groups => {:app => {:enabled => true}, :site => {:enabled => true}}
+default_attributes :active_groups => {:app => {:enabled => true}, :site => {:enabled => true}, :contractors => {:enabled => true}}
