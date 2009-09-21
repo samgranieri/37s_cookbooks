feaab02872947dfa97184dedd4abe54d91ff9588
Joshua Sierles <joshua@rasgueado.local>
2009-09-21 13:52:10 +0200

    clean up openbar role

--- roles/openbar.rb
+++ roles/openbar.rb
@@ -1,9 +1,7 @@
 name "openbar"
 description "Openbar"
-default_attributes :active_applications =>  {"open_bar" => {"env" =>  "production"}},
-
-:applications => {
-  :open_bar => {
-    :gems => ['fast_xs']
-  }
-}
+default_attributes :applications => {
+                      :open_bar => {
+                        :gems => ['fast_xs']
+                      }
+                    }
