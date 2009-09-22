ad0d898644ab78eec615c80ff732a8879bb70978
Joshua Sierles <joshua@rasgueado.local>
2009-09-22 12:20:13 +0200

    add pratik's key to app user only on web-staging

--- roles/web-staging.rb
+++ roles/web-staging.rb
@@ -1,7 +1,8 @@
 name "web-staging"
 description "Staging slice for smaller sites/apps"
 recipes "mysql::server", "php5::sites", "rails::apps"
-default_attributes :active_groups => {:contractors => {:enabled => true}},
+default_attributes :users => {:app => {:extra_ssh_keys => [:nagios, :solr, :pratik]}},
+                  :active_groups => {:contractors => {:enabled => true}},
                   "active_applications" => {"blogcabin" => {"env" => "staging"},
                  "bookstore" => {"env" => "staging"},
                  "jobs" => {"env" => "staging"},
