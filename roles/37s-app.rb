7c11b5d205053fe71907081b46a07dcb7bcef4a0
Joshua Sierles <joshua@rasgueado.local>
2009-09-22 13:26:40 +0200

    switch to default attributes for passenger on 37s slices

--- roles/37s-app.rb
+++ roles/37s-app.rb
@@ -3,7 +3,7 @@ description "General 37s application server"
 
 recipes "rails::apps", "php5::sites"
 
-override_attributes :active_applications => {
+default_attributes :active_applications => {
                      :bookstore => {"env" => "production"},
                      :blogcabin => {"env" => "production"},
                      :forum => {"env" => "production"},
@@ -25,7 +25,6 @@ override_attributes :active_applications => {
                    },
                    :active_groups => {:app => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}},
-
                    "apache" => {"listen_ports" => ["80","443"]},
                    "passenger" => {"max_pool_size" => "12", "use_global_queue" => "off", "rails_env" => "production", "max_instances_per_app" => "2"},
                    "php5" => {"version" => "5.2.9"},
