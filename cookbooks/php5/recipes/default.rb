caa0938dd4fb3b8645ec5d04c93e3dda0402d3fa
Mark Imbriaco <mark@imbriaco.com>
2009-09-23 16:25:06 -0400

    Use tar based PHP install again.

--- cookbooks/php5/recipes/default.rb
+++ cookbooks/php5/recipes/default.rb
@@ -4,6 +4,19 @@ unless node[:platform] == "ubuntu"
 end
 
 package "apache2-mpm-worker"
-package "php5-cli"
 package "libapache2-mod-fcgid"
 apache_module "fcgid"
+
+bash "install_php" do
+  code <<-EOC
+wget #{node[:php5][:dist_url]}
+tar -C /#{node[:php5][:path].split("/")[1..-2].join("/")} -xpf #{node[:php5][:tar_pkg]}
+EOC
+  user "root"
+  cwd "/tmp"
+  not_if { File.directory?(node[:php5][:path]) }
+end
+
+link "/usr/local/php/bin/php-cgi" do
+  to "/usr/bin/php-cgi"
+end
