bed5ae387f7de076ed8574739a9a76cc396201da
Mark Imbriaco <mark@imbriaco.com>
2009-09-25 00:56:21 -0400

    Add local patch for gitosis usernames to gitosis cookbook.

--- cookbooks/gitosis/recipes/default.rb
+++ cookbooks/gitosis/recipes/default.rb
@@ -9,9 +9,12 @@ group "gitosis" do
   action :remove
 end
 
-remote_file "/usr/share/python-support/gitosis/gitosis-0.2-py2.5.egg/gitosis/access.py" do
-  source "access.py"
-  owner "root"
-  group "root"
-  mode 0755
+# Install local modifications to Gitosis
+%W(access.py serve.py).each do |patched|
+  remote_file "/usr/share/python-support/gitosis/gitosis-0.2-py2.5.egg/gitosis/#{patched}" do
+    source "#{patched}"
+    owner "root"
+    group "root"
+    mode 0755
+  end
 end
