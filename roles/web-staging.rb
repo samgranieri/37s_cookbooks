7dbbebcfa72e6132d6fbe83cd453f2513cb5bb5b
Mark Imbriaco <mark@imbriaco.com>
2009-10-01 11:05:23 -0400

    Update web_staging for new mysql::server cookbook.

--- roles/web-staging.rb
+++ roles/web-staging.rb
@@ -2,17 +2,31 @@ name "web-staging"
 description "Staging slice for smaller sites/apps"
 recipes "mysql::server", "php5::sites", "rails::apps", "rails::delayed_job"
 default_attributes :users => {:app => {:extra_ssh_keys => [:nagios, :solr, :pratik]}},
-                  :active_groups => {:contractors => {:enabled => true}},
-                  "active_applications" => {"blogcabin" => {"env" => "staging"},
-                 "bookstore" => {"env" => "staging"},
-                 "jobs" => {"env" => "staging"},
-                 "help" => {"env" => "staging"},
-                 "extra_extra" => {"env" => "staging"},
-                 "haystack" => {"env" => "staging"},
-                 "openbar" => {"env" => "staging"},
-                 "champagne" => {"env" => "staging"}},
-                  "active_sites" => {"37signals" => {"env" => "staging"},
-                  "backpackit" => {"env" => "staging"},
-                  "highrisehq" => {"env" => "staging"}}
+                   :active_groups => {:contractors => {:enabled => true}},
+                   "active_applications" => {
+                     "blogcabin" => {"env" => "staging"},
+                     "bookstore" => {"env" => "staging"},
+                     "jobs" => {"env" => "staging"},
+                     "help" => {"env" => "staging"},
+                     "extra_extra" => {"env" => "staging"},
+                     "haystack" => {"env" => "staging"},
+                     "openbar" => {"env" => "staging"},
+                     "champagne" => {"env" => "staging"}
+                   },
+                   "active_sites" => {
+                     "37signals" => {"env" => "staging"},
+                     "backpackit" => {"env" => "staging"},
+                     "highrisehq" => {"env" => "staging"}
+                   },
+                   "mysql" => {
+                     "instances" => [
+                       { 
+                         "name" => "web_staging",
+                         "version" => "5.0.84-percona",
+                         "config" => { "innodb_buffer_pool_size" => "64M" }
+                       }
+                     ]
+                   }
+                   
 
   
