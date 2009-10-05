2cd4551fb50ff3964463a5013e30356a65f43545
Joshua Sierles <joshua@rasgueado.local>
2009-10-05 16:02:16 +0200

    guard against missing mysql instances variable

--- cookbooks/mysql/recipes/server.rb
+++ cookbooks/mysql/recipes/server.rb
@@ -1,11 +1,12 @@
+if node[:mysql][:instances]
+  node[:mysql][:instances].each do |instance|
+    mysql_server instance[:name] do
+      config instance[:config]
+      version instance[:version]
 
-node[:mysql][:instances].each do |instance|
-  mysql_server instance[:name] do
-    config instance[:config]
-    version instance[:version]
-
-    if instance[:backup_location]
-      backup_location instance[:backup_location]
+      if instance[:backup_location]
+        backup_location instance[:backup_location]
+      end
     end
   end
 end
