7a52ef83bd19f7f35271e831bb4c484f725443ca
Joshua Sierles <joshua@rasgueado.local>
2009-10-05 16:04:29 +0200

    set a default mysql instance for staging servers

--- roles/staging.rb
+++ roles/staging.rb
@@ -3,4 +3,14 @@ description "Staging server"
 recipes "rails::apps", "syslog::client", "nfs::client", "mysql::server", "nfs::client", "erlang", "haproxy", "memcached::basic"
 
 default_attributes :active_groups => {:app => {:enabled => true}},
-                   :active_sudo_groups => {:app => {:enabled => true}}
+                   :active_sudo_groups => {:app => {:enabled => true}},
+                   "mysql" => {
+                     "instances" => [
+                       { 
+                         "name" => "staging",
+                         "version" => "5.0.84-percona",
+                         "config" => { "innodb_buffer_pool_size" => "64M" }
+                       }
+                     ]
+                   }
+                   
