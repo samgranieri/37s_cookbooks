fe0d1f3af8e21f5ac0cc950961f450b66436d83a
Joshua Sierles <joshua@rasgueado.local>
2009-10-05 15:57:10 +0200

    use unicorn for haystack

--- roles/haystack.rb
+++ roles/haystack.rb
@@ -1,9 +1,11 @@
 name "haystack"
 description "Haystack"
-default_attributes :applications => {
+override_attributes :active_applications => {"haystack" => {"env" => "production"}},
+                   :applications => {
                      :haystack => {
                        :gems => ['fast_xs', 'xml-simple', 'right_aws', 'bcrypt-ruby', 'erubis'],
                        :packages => ["imagemagick"]
                      }
                    },
-                   :delayed_jobs => {:enabled => true, :rails_env => "staging", :app_name => "haystack"}
+                   :delayed_jobs => {:enabled => true, :rails_env => "staging", :app_name => "haystack"},
+                   :rails => {:web_server => "nginx", :app_server => "unicorn"}
