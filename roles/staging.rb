75609ecb6958817212847275c3f1714a7c38b387
Joshua Sierles <joshua@rasgueado.local>
2009-10-05 18:43:26 +0200

    remove NFS dependendency

--- roles/staging.rb
+++ roles/staging.rb
@@ -1,6 +1,6 @@
 name "staging"
 description "Staging server"
-recipes "rails::apps", "syslog::client", "nfs::client", "mysql::server", "nfs::client", "erlang", "haproxy", "memcached::basic"
+recipes "rails::apps", "syslog::client", "mysql::server", "erlang", "haproxy", "memcached::basic"
 
 default_attributes :active_groups => {:app => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}},
