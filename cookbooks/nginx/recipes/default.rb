7216d2f030578d262a0c6f7102ee069739be1ad9
Joshua Sierles <joshua@rasgueado.local>
2009-10-06 02:50:44 +0200

    restart nginx on config changes

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,5 +1,9 @@
 package "nginx"
 
+service "nginx" do
+  supports :status => true, :restart => true, :reload => true
+end
+
 directory node[:nginx][:log_dir] do
   mode 0755
   owner node[:nginx][:user]
@@ -21,16 +25,9 @@ template "nginx.conf" do
   owner "root"
   group "root"
   mode 0644
-end
-
-template "#{node[:nginx][:dir]}/sites-available/default" do
-  source "default-site.erb"
-  owner "root"
-  group "root"
-  mode 0644
+  notifies :reload, resources(:service => "nginx")
 end
 
 service "nginx" do
-  supports :status => true, :restart => true, :reload => true
   action [ :enable, :start ]
 end
