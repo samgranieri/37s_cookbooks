3dea0cc00ee8062944a28fd958f6b74ad588d561
Mark Imbriaco <mark@imbriaco.com>
2009-10-09 14:54:28 -0400

    s/yubico/yubikey/.

--- roles/yubikey.rb
+++ roles/yubikey.rb
@@ -2,4 +2,4 @@ name "yubikey"
 description "Yubikey Validation/Management Server"
 recipes "php5::sites"
 
-default_attributes :active_sites => { :yubico => { "env" => "production" }}
+default_attributes :active_sites => { :yubikey => { "env" => "production" }}
