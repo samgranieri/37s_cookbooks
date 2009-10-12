482744df3ab2b080ec5c519ad6b7186e89bab2c4
Mark Imbriaco <mark@imbriaco.com>
2009-10-12 11:34:44 -0400

    Add cron jobs to the backup role.

--- roles/backup.rb
+++ roles/backup.rb
@@ -1,6 +1,6 @@
 name "backup"
 description "Backup Server"
-recipes "backup::server", "rsnapshot::server", "nfs::server", "java", "gnupg"
+recipes "backup::server", "rsnapshot::server", "nfs::server", "java", "gnupg", "cron"
 
 default_attributes :active_groups => {:storage => {:enabled => true}},
                    :rsnapshot =>  {
@@ -8,5 +8,39 @@ default_attributes :active_groups => {:storage => {:enabled => true}},
                        "storage@git:/u/git/" => {:target => "git"},
                        "storage@qb-01:/u/apps/queenbee/shared/log/" => {:target => "qb-01"},
                        "storage@qb-02:/u/apps/queenbee/shared/log/" => {:target => "qb-02"}
+                     }
+                   },
+                   :cron => {
+                     :jobs => {
+                       :cloudfiles_upload => {
+                         :command => '/root/cloudput.sh',
+                         :minute => '0',
+                         :hour => '11',
+                         :user => 'root'
+                       },
+                       :cloudfiles_cleanup => {
+                         :command => '/root/cloudsweep.sh',
+                         :minute => '0',
+                         :hour => '10',
+                         :user => 'root'
+                       },
+                       :rsnapshot_hourly => {
+                         :command => '/usr/bin/rsnapshot hourly',
+                         :minute => '0',
+                         :hour => '*/4',
+                         :user => 'root'
+                       },
+                       :rsnapshot_daily => {
+                         :command => '/usr/bin/rsnapshot daily',
+                         :minute => '30',
+                         :hour => '23',
+                         :user => 'root'
+                       },
+                       :rotate_db_backups => {
+                         :command => '/usr/local/bin/rotate-db-backups',
+                         :minute => '0',
+                         :hour => '15',
+                         :user => 'root'
+                       }
                      }
                    }
