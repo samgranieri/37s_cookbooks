046da3efea289f4a78441e7d89042abc4e110793
Joshua Sierles <joshua@rasgueado.local>
2009-10-14 12:57:09 +0200

    remove nfs client cookbook from cron

--- roles/cron.rb
+++ roles/cron.rb
@@ -1,5 +1,5 @@
 name "cron"
 description "Cron/utility server"
-recipes "cron", "nfs::client", "syslog::client", "rails::app_dependencies"
+recipes "cron", "syslog::client", "rails::app_dependencies"
 default_attributes :active_groups => {:app => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}}
