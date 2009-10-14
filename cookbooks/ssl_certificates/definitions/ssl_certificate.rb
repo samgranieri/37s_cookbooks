c439f735a87b76015dbee9c74e6995db8b36eca5
Joshua Sierles <joshua@rasgueado.local>
2009-10-14 12:53:05 +0200

    add cookbook for SSL certificate distribution

---
+++ cookbooks/ssl_certificates/definitions/ssl_certificate.rb
@@ -0,0 +1,14 @@
+define :ssl_certificate do
+  remote_file "#{node[:ssl_certificates][:path]}/#{params[:name]}.crt" do
+    source "#{params[:name]}.crt"
+    mode 750
+    owner "root"
+    group "www-data"
+  end
+  remote_file "#{node[:ssl_certificates][:path]}/#{params[:name]}.key" do
+    source "#{params[:name]}.key"
+    mode 750
+    owner "root"
+    group "www-data"
+  end
+end
