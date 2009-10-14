b90671538550a8e19c9801887de578b36c297ae9
Joshua Sierles <joshua@rasgueado.local>
2009-10-14 12:54:22 +0200

    add haystack domain for ssl certs

--- roles/haystack.rb
+++ roles/haystack.rb
@@ -3,6 +3,7 @@ description "Haystack"
 default_attributes :active_applications => {"haystack" => {"env" => "production"}},
                    :applications => {
                      :haystack => {
+                       :domains => ["haystack.com"],
                        :gems => ['fast_xs', 'xml-simple', 'right_aws', 'bcrypt-ruby', 'erubis', 'aws-s3'],
                        :packages => ["imagemagick"]
                      }
