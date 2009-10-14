29260c2981d30105d99e0480d20746770bfe1c17
Joshua Sierles <joshua@rasgueado.local>
2009-10-14 12:54:03 +0200

    add ssl_certificates on app slices

--- roles/app.rb
+++ roles/app.rb
@@ -1,6 +1,6 @@
 name "app"
 description "Basic Rails application server"
-recipes "rails::apps", "syslog::client", "nfs::client"
+recipes "rails::apps", "ssl_certificates", "syslog::client", "nfs::client"
 
 default_attributes :active_groups => {:app => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}}
