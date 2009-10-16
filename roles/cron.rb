641174e41ca15f46955d3a69d0da59d3453f4966
Joshua Sierles <joshua@rasgueado.local>
2009-10-16 16:58:22 +0200

    add delayed jobs to cron

--- roles/cron.rb
+++ roles/cron.rb
@@ -1,5 +1,5 @@
 name "cron"
 description "Cron/utility server"
-recipes "cron", "syslog::client", "rails::app_dependencies"
+recipes "cron", "syslog::client", "rails::app_dependencies", "rails::delayed_job"
 default_attributes :active_groups => {:app => {:enabled => true}},
                    :active_sudo_groups => {:app => {:enabled => true}}
