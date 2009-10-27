c84d54cd188e6e8152cfdab275d970631048f9c9
Joshua Sierles <joshua@rasgueado.local>
2009-10-27 17:05:13 +0100

    add mail relay haproxy instance

--- roles/proxy.rb
+++ roles/proxy.rb
@@ -2,4 +2,30 @@ name "proxy"
 description "Proxy server"
 recipes "haproxy", "keepalived"
 
-default_attributes :active_groups => {:app => {:enabled => true}}
+default_attributes :active_groups => {:app => {:enabled => true}},
+:haproxy => {
+  :instances => {
+    :mail_relay => {
+      :listeners => {
+        :mail_relay_pool => {
+          :ip => "0.0.0.0",
+          :port => "22001",
+          :mode => "tcp",
+          :admin_port => "22002",
+          :backends => {
+            "mail-relay-01" => {
+              :ip => "192.168.2.139",
+              :port => "25",
+              :options => "check"
+            },
+            "mail-relay-02" => {
+              :ip => "192.168.2.141",
+              :port => "25",
+              :options => "check"
+            }
+          }
+        }
+      }
+    }
+  }
+}
