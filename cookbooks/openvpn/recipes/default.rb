69cf94bc97a8e368123926a7ebba1d7a7a3b1646
Joshua Sierles <joshua@rasgueado.local>
2009-11-06 12:19:09 +0100

    fix file mode for openvpn binaries

--- cookbooks/openvpn/recipes/default.rb
+++ cookbooks/openvpn/recipes/default.rb
@@ -1,22 +1,3 @@
-#
-# Cookbook Name:: openvpn
-# Recipe:: default
-#
-# Copyright 2009, Opscode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 package "openvpn" do
   action :install
 end
@@ -41,7 +22,7 @@ remote_directory "/etc/openvpn/easy-rsa" do
   files_backup 0
   files_owner "root"
   files_group "root"
-  files_mode 0600
+  files_mode 0700
   owner "root"
   group "root"
   mode 0700
