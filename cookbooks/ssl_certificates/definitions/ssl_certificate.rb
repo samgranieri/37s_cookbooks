bb299acf2d376e1e898331d9c2498a6f652936aa
Joshua Sierles <joshua@165.red-88-31-209.staticip.rima-tde.net>
2009-11-09 14:10:05 +0100

    add 37signals wildcard certs

--- cookbooks/ssl_certificates/definitions/ssl_certificate.rb
+++ cookbooks/ssl_certificates/definitions/ssl_certificate.rb
@@ -1,13 +1,23 @@
-define :ssl_certificate do
-  remote_file "#{node[:ssl_certificates][:path]}/#{params[:name]}.crt" do
-    source "#{params[:name]}.crt"
+define :ssl_certificate, :wildcard => false do
+  
+  name = params[:wildcard] ? "#{params[:name]}_wildcard" : params[:name]
+  
+  remote_file "#{node[:ssl_certificates][:path]}/#{name}.crt" do
+    source "#{name}.crt"
     mode "0640"
     cookbook "ssl_certificates"
     owner "root"
     group "www-data"
   end
-  remote_file "#{node[:ssl_certificates][:path]}/#{params[:name]}.key" do
-    source "#{params[:name]}.key"
+  remote_file "#{node[:ssl_certificates][:path]}/#{name}.key" do
+    source "#{name}.key"
+    mode "0640"
+    cookbook "ssl_certificates"
+    owner "root"
+    group "www-data"
+  end
+  remote_file "#{node[:ssl_certificates][:path]}/#{name}_intermediate.crt" do
+    source "#{name}_intermediate.crt"
     mode "0640"
     cookbook "ssl_certificates"
     owner "root"
