91dc807f88d879c0a64ae9cc75ec88eb5ed18127
Joshua Sierles <joshua@210.red-79-149-201.staticip.rima-tde.net>
2009-11-12 12:31:48 +0100

    set domains for writeboard

--- roles/writeboard.rb
+++ roles/writeboard.rb
@@ -3,6 +3,7 @@ description "Writeboard"
 default_attributes :active_applications =>  {"writeboard" =>  {"env" =>  "production"}},
                    :applications => {
                      :writeboard => {
+                       :domains => ["123.writeboard.com"],
                        :gems => ['fast_xs', 'RedCloth'],
                        :packages => ['elinks'],
                        :symlinks => {'/usr/local/bin/elinks' => '/usr/bin/elinks'} 
