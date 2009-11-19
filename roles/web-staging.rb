234de5f06b1ea00640afa985e6f6477796ee3a59
Joshua Sierles <joshua@rasgueado.local>
2009-11-19 21:39:23 +0100

    add nginx to web-staging

--- roles/web-staging.rb
+++ roles/web-staging.rb
@@ -1,6 +1,6 @@
 name "web-staging"
 description "Staging slice for smaller sites/apps"
-recipes "mysql::server", "php5::sites", "rails::apps", "rails::delayed_job"
+recipes "mysql::server", "php5::sites", "rails::apps", "rails::delayed_job", "nginx"
 override_attributes :users => {:app => {:password => "$1$hk40k332$wpSYJYlbbVo3AK/7thxO3.", :comment => "App User", :uid => 1003, :groups => [:app], :ssh_key_groups => [:app,:site,:admin], :extra_ssh_keys => [:nagios, :solr, :pratik]}},
                    "active_applications" => {
                      "blogcabin" => {"env" => "staging"},
