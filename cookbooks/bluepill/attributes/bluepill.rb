706f143ec64c7293070274ba101ded1eda7a72c1
Joshua Sierles <joshua@rasgueado.local>
2009-11-22 21:52:39 +0100

    add bluepill init script

--- cookbooks/bluepill/attributes/bluepill.rb
+++ cookbooks/bluepill/attributes/bluepill.rb
@@ -1,1 +1,2 @@
 default.bluepill[:version] = "0.0.21"
+default.bluepill[:config_dir] = "/etc/bluepill"
--- cookbooks/bluepill/recipes/default.rb
+++ cookbooks/bluepill/recipes/default.rb
@@ -2,20 +2,12 @@ gem_package "bluepill" do
   version node[:bluepill][:version]
 end
 
-directory "/etc/bluepill" do
-  owner "root"
-  group "root"
-  mode 0755
+directory node[:bluepill][:config_dir]
+directory node[:bluepill][:log_dir]
+directory node[:bluepill][:pid_dir]
+
+template "/etc/init.d/bluepill" do
+  source "init.sh.erb"
 end
 
-directory "/var/log/bluepill" do
-  owner "root"
-  group "root"
-  mode 0755
-end
-
-directory "/var/run/bluepill" do
-  owner "root"
-  group "root"
-  mode 0755
-end
+service "bluepill"
