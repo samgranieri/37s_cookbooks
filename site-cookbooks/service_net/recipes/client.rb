5755362e9f2ad4dca46caf072ccea4e5205c043b
Mark Imbriaco <mark@imbriaco.com>
2009-11-25 13:07:26 -0500

    Fix attribute references

--- site-cookbooks/service_net/recipes/client.rb
+++ site-cookbooks/service_net/recipes/client.rb
@@ -2,6 +2,6 @@
 route "10.191.192.0" do
   route_type :net
   netmask "255.255.192.0"
-  gateway service_net[:gateway_ip]
+  gateway node[:service_net][:gateway_ip]
   metric 1
 end
--- site-cookbooks/service_net/recipes/gateway.rb
+++ site-cookbooks/service_net/recipes/gateway.rb
@@ -2,6 +2,6 @@
 require_recipe "iptables"
 
 iptables_rule "service_net" do
-  service_interface service_net[:service_interface]
-  local_interface service_net[:local_interface]
+  service_interface node[:service_net][:service_interface]
+  local_interface node[:service_net][:local_interface]
 end
