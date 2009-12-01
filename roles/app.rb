4bd34fd4b17c4efabc0c0480795834c3e30e9ea4
Joshua Sierles <joshua@rasgueado.local>
2009-12-01 13:01:30 +0100

    move examples to their own directory

--- roles/app.rb
+++
@@ -1,7 +0,0 @@
-# Example role for a Rails application server with nfs mounts and remote syslogging
-name "app"
-description "Basic Rails application server"
-recipes "rails::apps", "syslog::client", "nfs::client"
-
-default_attributes :active_groups => {:app => {:enabled => true}},
-                   :active_sudo_groups => {:app => {:enabled => true}}
--- roles/dns.rb
+++
@@ -1,4 +0,0 @@
-# Ezample dns server role using search indexes to generate DNS entries
-name "dns"
-description "DNS server"
-recipes "djbdns::cache", "djbdns::internal_server", "djbdns::autozone"
