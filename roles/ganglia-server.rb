06092bf3ac93f6988ee36cebe9b17968eeaaf091
Mark Imbriaco <mark@imbriaco.com>
2009-11-30 22:41:46 -0500

    Add a ganglia-server role.

---
+++ roles/ganglia-server.rb
@@ -0,0 +1,4 @@
+name "ganglia-server"
+description "Ganglia System Monitoring Server"
+recipe "ganglia::server"
+
--- roles/ganglia.rb
+++ roles/ganglia.rb
@@ -1,9 +1,10 @@
 name "ganglia"
 description "Ganglia System Monitoring"
+
 default_attributes :ganglia => {
-  :cluster_name => '37signals',
-  :clusters => [
-                { :name => 'Basecamp', :interval => 60, :nodes => [ 'bc-01', 'bc-02', 'bc-03', 'bc-04' ] }
-               ],
-  :servers  => [ '192.168.1.159:3737', '192.168.1.153:3737' ]
-}
+                     :cluster_name => '37signals',
+                     :clusters => [
+                       { :name => 'Basecamp', :interval => 60, :nodes => [ 'bc-01', 'bc-02', 'bc-03', 'bc-04' ] }
+                     ],
+                     :servers  => [ '192.168.1.159:3737', '192.168.1.153:3737' ]
+                   }
