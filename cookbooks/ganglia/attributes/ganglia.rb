0910d60ad6fd3d53cfbd4575d9447e26f7f9ce7c
Mark Imbriaco <mark@imbriaco.com>
2009-11-30 22:30:45 -0500

    Add basic Ganglia cookbook

---
+++ cookbooks/ganglia/attributes/ganglia.rb
@@ -0,0 +1,2 @@
+default[:ganglia][:server_fqdn] = 'ganglia.rack-dfw-int.37signals.com'
+default[:ganglia][:clusters] = []
---
+++ cookbooks/ganglia/metadata.rb
@@ -0,0 +1,5 @@
+maintainer        "ganglia"
+maintainer_email  "sysadmins@37signals.com"
+description       "Configures Ganglia"
+version           "0.1"
+depends           "apache2"
